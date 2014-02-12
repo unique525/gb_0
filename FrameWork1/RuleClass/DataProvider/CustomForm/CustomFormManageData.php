e100068a42c70123462c281c4ed0dab3e0a54756
525 <76349186@qq.com>
2014-02-12 17:38:46 +0800

    w

--- FrameWork1/RuleClass/DataProvider/CustomForm/CustomFormManageData.php
+++ FrameWork1/RuleClass/DataProvider/CustomForm/CustomFormManageData.php
@@ -26,14 +26,14 @@ class CustomFormManageData extends BaseManageData {
     /**
      * 修改
      * @param array $httpPostData $_post数组
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return int 执行结果
      */
-    public function Modify($httpPostData,$tableIdValue) {
+    public function Modify($httpPostData,$customFormId) {
         $result=-1;
         $dataProperty = new DataProperty();
         if(!empty($httpPostData)){
-            $sql = parent::GetUpdateSql($httpPostData,self::TableName_CustomForm, self::TableId_CustomForm, $tableIdValue, $dataProperty);
+            $sql = parent::GetUpdateSql($httpPostData,self::TableName_CustomForm, self::TableId_CustomForm, $customFormId, $dataProperty);
             $result = $this->dbOperator->Execute($sql, $dataProperty);
         }
         return $result;
@@ -41,109 +41,109 @@ class CustomFormManageData extends BaseManageData {
 
     /**
      * 获取自定义页面分页列表
-     * @param int $documentChannelId 频道id
+     * @param int $channelId 频道id
      * @param int $pageBegin 起始页码
      * @param int $pageSize 每页大小
      * @param int $allCount 总大小
      * @return array 表单数据集
      */
-    public function GetListPager($documentChannelId, $pageBegin, $pageSize, &$allCount) {
+    public function GetListPager($channelId, $pageBegin, $pageSize, &$allCount) {
         $searchSql = "";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("DocumentChannelId", $documentChannelId);
+        $dataProperty->AddField("ChannelId", $channelId);
 
         $sql = "
             SELECT
             *
             FROM
             " . self::TableName_CustomForm . "
-            WHERE DocumentChannelId=:DocumentChannelId AND State<100 " . $searchSql . " ORDER BY Sort DESC, CreateDate DESC LIMIT " . $pageBegin . "," . $pageSize . "";
+            WHERE ChannelId=:ChannelId AND State<100 " . $searchSql . " ORDER BY Sort DESC, CreateDate DESC LIMIT " . $pageBegin . "," . $pageSize . "";
 
 
         $result = $this->dbOperator->ReturnArray($sql, $dataProperty);
-        $sqlCount = "SELECT count(*) FROM " . self::TableName_CustomForm . " WHERE DocumentChannelId=:DocumentChannelId AND state<100 " . $searchSql;
-        $allCount = $this->dbOperator->ReturnInt($sqlCount, $dataProperty);
+        $sqlCount = "SELECT count(*) FROM " . self::TableName_CustomForm . " WHERE ChannelId=:ChannelId AND state<100 " . $searchSql;
+        $allCount = $this->dbOperator->GetInt($sqlCount, $dataProperty);
         return $result;
     }
 
     /**
      * 获取管理员ID
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return int type 取得的管理员id
      */
-    public function GetAdminUserID($tableIdValue) {
+    public function GetManageUserId($customFormId) {
         $sql = "SELECT AdminUserId FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
-        $result = $this->dbOperator->ReturnInt($sql, $dataProperty);
+        $dataProperty->AddField("CustomFormId", $customFormId);
+        $result = $this->dbOperator->GetInt($sql, $dataProperty);
         return $result;
     }
 
     /**
      * 通过ID获取一条记录
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return array 表单数据
      */
-    public function GetOne($tableIdValue) {
+    public function GetOne($customFormId) {
         $sql = "SELECT * FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
+        $dataProperty->AddField("CustomFormId", $customFormId);
         $result = $this->dbOperator->ReturnRow($sql, $dataProperty);
         return $result;
     }
 
     /**
      * 改变状态
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @param int $state 状态
      * @return int 执行结果
      */
 
-    public function ChangeState($tableIdValue, $state) {
+    public function ModifyState($customFormId, $state) {
         $sql = "UPDATE " . self::TableName_CustomForm . " SET State = :State WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
         $dataProperty->AddField("State", $state);
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
+        $dataProperty->AddField("CustomFormId", $customFormId);
         $result = $this->dbOperator->Execute($sql, $dataProperty);
         return $result;
     }
 
     /**
      * 获取表单状态
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return int 表单状态
      */
-    public function GetState($tableIdValue) {
+    public function GetState($customFormId) {
         $sql = "SELECT State FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
-        $result = $this->dbOperator->ReturnInt($sql, $dataProperty);
+        $dataProperty->AddField("CustomFormId", $customFormId);
+        $result = $this->dbOperator->GetInt($sql, $dataProperty);
         return $result;
     }
 
     /**
      * 获取创建日期
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return string 表单创建日期
      */
-    public function GetCreateDate($tableIdValue) {
+    public function GetCreateDate($customFormId) {
         $sql = "SELECT CreateDate FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
+        $dataProperty->AddField("CustomFormId", $customFormId);
         $result = $this->dbOperator->ReturnString($sql, $dataProperty);
         return $result;
     }
 
     /**
      * 返回表单所在频道ID
-     * @param int $tableIdValue 表单id
+     * @param int $customFormId 表单id
      * @return int 表单所在频道id
      */
-    public function GetDocumentChannelID($tableIdValue) {
-        $sql = "SELECT DocumentChannelId FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
+    public function GetChannelId($customFormId) {
+        $sql = "SELECT ChannelId FROM " . self::TableName_CustomForm . " WHERE CustomFormId = :CustomFormId";
         $dataProperty = new DataProperty();
-        $dataProperty->AddField("CustomFormId", $tableIdValue);
-        $result = $this->dbOperator->ReturnInt($sql, $dataProperty);
+        $dataProperty->AddField("CustomFormId", $customFormId);
+        $result = $this->dbOperator->GetInt($sql, $dataProperty);
         return $result;
     }
 
