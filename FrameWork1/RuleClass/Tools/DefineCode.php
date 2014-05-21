026a5bb703062466e938b2225d7012bee9a84db2
Ljunyi <273070025@qq.com>
2014-05-21 17:00:17 +0800

    aa

--- FrameWork1/RuleClass/Tools/DefineCode.php
+++ FrameWork1/RuleClass/Tools/DefineCode.php
@@ -28,6 +28,53 @@ class DefineCode {
      */
     const  DATA_CACHE_CODE = -104000;
 
+    /**
+     * 会员 后台管理 生成类
+     */
+    const USER_MANAGE = -201000;
+
+    /**
+     * 会员 前台 生成类
+     */
+    const USER_PUBLIC = -202000;
+
+    /**
+     * 会员 客户端 返回值前缀
+     */
+    const USER_CLIENT = -203000;
+
+    /**
+     * 会员相册 后台管理 返回值前缀
+     */
+    const USER_ALBUM_MANAGE = -204000;
+
+    /**
+     * 会员相册 客户端 返回值前缀
+     */
+    const USER_ALBUM_CLIENT = -205000;
+
+    /**
+     * 会员相册 前台 返回值前缀
+     */
+    const USER_ALBUM_PUBLIC = -206000;
+
+    /**
+     * 会员相册 照片 后台管理 返回值前缀
+     */
+    const USER_ALBUM_PIC_MANAGE = -207000;
+
+    /**
+     * 会员相册 照片 客户端 返回值前缀
+     */
+    const USER_ALBUM_PIC_CLIENT = -208000;
+
+    /**
+     * 会员相册 照片 前台 返回值前缀
+     */
+    const USER_ALBUM_PIC_PUBLIC = -209000;
+
+
+
 
 
 } 
