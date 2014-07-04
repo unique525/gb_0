d21e7986a043e3cfd2a4e5a5c0e5c791489cfd47
hyyjy <8682015@163.com>
2014-07-04 15:24:57 +0800

    123

--- system_template/common/vote/vote_select_item_add_count_ratio.html.php
+++ system_template/common/vote/vote_select_item_add_count_ratio.html.php
@@ -1,64 +1,59 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <link type="text/css" href="{rootpath}/images/common.css" rel="stylesheet" />
-        <script type="text/javascript" src="http://www.image1.cn/common/js/jquery-1.5.1.min.js"></script>
-        <script type="text/javascript" src="{rootpath}/js/common.js"></script>
-        <script type="text/javascript" src="{rootpath}/common/js/jquery-ui-1.8.17.custom.min.js"></script>
-        <link href="{rootpath}/common/css/jquery-ui-1.8.17.custom.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+{common_head}
+
+<script type="text/javascript" language="JavaScript">
+    <!--
     function calculate(id){
-        var itemcount=$(".griditem").size();
-        var recordcount=0;
-        var addcount=0;
-        var allcount=0;
+        var itemCount=parseInt($(".grid_item").size());
+        var recordCount=0;
+        var addCount=0;
         if(id>0){
-            for(var i=1;i<=itemcount;i++){
+            for(var i=0;i <= itemCount;i++){
                 if(i!=id){
-                    var addother=parseInt($("#num_"+id).val())/parseFloat($("#per_"+id).html())*parseFloat($("#per_"+i).html());
-                    $("#addcount_"+i).val(parseInt($("#addcount_"+i).val())+parseInt(addother));
+                    var addOther=parseInt($("#num_"+id).val())/parseFloat($("#per_"+id).html())*parseFloat($("#per_"+i).html());
+                    $("#AddCount_"+i).val(parseInt($("#AddCount_"+i).val())+parseInt(addOther));
                 }else{
-                    $("#addcount_"+i).val(parseInt($("#num_"+i).val())+parseInt($("#addcount_"+i).val()))                     
+                    $("#AddCount_"+i).val(parseInt($("#num_"+i).val())+parseInt($("#AddCount_"+i).val()))
                 }
             }
         }
-        for(var i=1;i<=itemcount;i++){
-            recordcount+=parseInt($("#recordcount_"+i).html());
-            addcount+=parseInt($("#addcount_"+i).val());
+        for(var i=1;i<=itemCount;i++){
+            recordCount+=parseInt($("#RecordCount_"+i).html());
+            addCount+=parseInt($("#AddCount_"+i).val());
         }
-        allcount=recordcount+addcount;
-        for(var i=1;i<=itemcount;i++){
-            $("#per_"+i).html(((parseInt($("#recordcount_"+i).html())+parseInt($("#addcount_"+i).val()))/allcount*100).toFixed(1))
+        for(var i=1;i<=itemCount;i++){
+            $("#per_"+i).html(((parseInt($("#RecordCount_"+i).html())+parseInt($("#AddCount_"+i).val()))/allCount*100).toFixed(1))
         }
     }
         $().ready(function () {
-		calculate();
+		calculate(0);
         });
         
         function sub(){
-            var itemcount=$(".griditem").size();
-            result=0;
-            for(var i=1;i<=itemcount;i++){
-                if(parseInt($("#recordcount_"+i).html())+parseInt($("#addcount_"+i).html())<0){
+            var itemCount=$(".grid_item").size();
+            var result=0;
+            for(var i=1;i<=itemCount;i++){
+                if((parseInt($("#RecordCount_"+i).html())+parseInt($("#AddCount_"+i).html()))<0){
                     result=1;
                 }
             }
             if(result>0){
                 alert('数据错误！有票数为负');
             }else{
-                $('#add_together').submit();
+                $('#mainForm').submit();
             }
         }
+    -->
 </script>
-    </head>
-    <body>
-
-<div style="margin:8px;hasvisdata">
-
-<table class="docgrid" cellpadding="0" cellspacing="0">
-<tr class="gridtitle">
+</head>
+<body>
+<form id="mainForm" method="post" enctype="multipart/form-data"  action="/vote/index.php?a=voteselectitemmanage&m={method}&voteid={voteid}&voteitemid={voteitemid}">
+<table class="grid" cellpadding="0" cellspacing="0">
+<tr class="grid_title">
 <td style="width:60px;text-align: center;"></td>
 <td style="width:10px;text-align: center;"></td>
 <td style="text-align: center;">选项名称</td>
@@ -67,30 +62,30 @@
 <td style="width:60px; text-align:center;">百分比</td>
 <td style="width:60px;text-align:center;">选项ID</td>
 </tr>
-        <form id="add_together" method="post" enctype="multipart/form-data"  action="/vote/index.php?a=voteselectitemmanage&m={method}&voteid={voteid}&voteitemid={voteitemid}">
-<cscms id="voteselectitemlist" type="list">
+<icms id="vote_select_item_list" type="list">
 <item>
 <![CDATA[
-<tr class="griditem">
+<tr class="grid_item">
 <td style="text-align: center;"><input id="num_{c_no}" type="text" style="width:60px"/></td>
 <td style="text-align: center;"><input id="add_{c_no}" type="button" value="加" onclick='calculate({c_no})' /></td>
-<td class="speline" style="text-align: center;" >{f_voteselectitemtitle}</td>
-<td class="speline" style="text-align: center;" id="recordcount_{c_no}">{f_recordcount}</td>
-<td class="speline" style="text-align: center;">
-    <input class="input" id="addcount_{c_no}" name="addcount_{c_no}" type="text"   value="{f_addcount}" style="width:60px"/>
-    <input class="input" name="id_{c_no}" type="text"   value="{f_voteselectitemid}" style="width:60px;display:none"/>
+<td class="spe_line" style="text-align: center;" >{f_VoteSelectItemTitle}</td>
+<td class="spe_line" style="text-align: center;" id="RecordCount_{c_no}">{f_RecordCount}</td>
+<td class="spe_line" style="text-align: center;">
+    <input class="input" id="AddCount_{c_no}" name="AddCount_{c_no}" type="text"   value="{f_AddCount}" style="width:60px"/>
+    <input class="input" name="id_{c_no}" type="text"   value="{f_VoteSelectItemId}" style="width:60px;display:none"/>
 </td>
-<td class="speline" style="text-align: center;" id="per_{c_no}"></td>
-<td class="speline" style="text-align: center;">{f_voteselectitemid}</td>
+<td class="spe_line" style="text-align: center;" id="per_{c_no}"></td>
+<td class="spe_line" style="text-align: center;">{f_VoteSelectItemId}</td>
 </tr>
 ]]>
 </item>
-</cscms>
-        <tr>
-            <td><input style="margin-top: 30px" value="提交" class="btn" type="button" onclick="sub()" /></td>
-        </tr>
-        </form>
+</icms>
 </table>
-</div>
-    </body>
+<table class="grid" cellpadding="0" cellspacing="0">
+<tr>
+<td><input style="margin-top: 30px" value="提交" class="btn" type="button" onclick="sub()" /></td>
+</tr>
+</table>
+</form>
+</body>
 </html>
