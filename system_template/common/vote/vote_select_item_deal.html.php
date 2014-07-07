eaa548f3ca60de11560251f0d5cc3b79c2d5607b
hyyjy <8682015@163.com>
2014-07-07 13:23:41 +0800

    123

--- system_template/common/vote/vote_select_item_deal.html.php
+++ system_template/common/vote/vote_select_item_deal.html.php
@@ -26,10 +26,11 @@
             });
             function sub()
             {
-                if($('#f_VoteSelectItemTitle').val() == ''){
+                var voteSelectItemTitle = $('#f_VoteSelectItemTitle');
+                if(voteSelectItemTitle.val() == ''){
                     alert('请填写选项名称！');
                 }
-                else if($('#f_VoteSelectItemTitle').val().length>500){
+                else if(voteSelectItemTitle.val().length>500){
                     alert('选项名称过长请不要超过500个字节！');
                 }
                 else {$('#mainForm').submit();}
@@ -47,7 +48,7 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="spe_line" height="30" align="right">选项名称：</td>
+                    <td class="spe_line" height="30" align="right"><label for="f_VoteSelectItemTitle">选项名称：</label></td>
                      <td class="spe_line"><input name="f_VoteSelectItemTitle" id="f_VoteSelectItemTitle" value="{VoteSelectItemTitle}" type="text" class="input_box" style=" width: 300px;" /></td>
                 </tr>
                 <tr>
@@ -55,7 +56,7 @@
                     <td class="spe_line" style="text-align: left">
                         <input id="file_title_pic" name="file_title_pic" type="file" class="input_box" style="width:300px; background: #ffffff; margin-top: 3px;" /> <span id="preview_title_pic" style="cursor:pointer">[预览]</span>
                         <div id="dialog_title_pic" title="题图预览（{TitlePic}）" style="display:none;">
-                            <div id="pubtable">
+                            <div id="pub_table">
                                 <table>
                                     <tr>
                                         <td><img id="img_title_pic" src="{TitlePic}" alt="title_pic" /></td>
@@ -66,21 +67,22 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="spe_line" style="width:200px;height:35px;text-align: right;">直接转向网址：</td>
+                    <td class="spe_line" style="width:200px;height:35px;text-align: right;"><label for="f_DirectUrl">直接转向网址:</label></td>
                     <td class="spe_line" style="text-align: left"><input type="text" class="input_box" id="f_DirectUrl" name="f_DirectUrl" value="{DirectUrl}" style=" width: 600px;font-size:14px;" maxlength="200" /> (设置直接转向网址后，文档将直接转向到该网址)</td>
                 </tr>
                 <tr>
-                    <td class="spe_line" height="30" align="right">排序：</td>
-                    <td class="spe_line"><input name="f_sort" id="f_sort" value="{sort}" type="text" class="input_number" style=" width: 60px;" />(注:输入数字,数值越大越靠前)</td>
+                    <td class="spe_line" height="30" align="right"><label for="f_Sort">排序：</label></td>
+                    <td class="spe_line"><input name="f_Sort" id="f_Sort" value="{Sort}" type="text" class="input_number" style=" width: 60px;" />(注:输入数字,数值越大越靠前)</td>
                 </tr>
 
                 <tr>
-                    <td class="spe_line" height="30" align="right">状态：</td>
+                    <td class="spe_line" height="30" align="right"><label for="f_State">状态：</label></td>
                     <td class="spe_line">
                         <select id="f_State" name="f_State">
-                            <option value="0" {s_State_0}>启用</option>
-                            <option value="100" {s_State_100}>停用</option>
+                            <option value="0">启用</option>
+                            <option value="100">停用</option>
                         </select>
+                        {s_State}
                     </td>
                 </tr>
                 <tr>
@@ -88,7 +90,7 @@
                    <td class="spe_line">{RecordCount}</td>
                 </tr>
                 <tr>
-                   <td class="spe_line" height="30" align="right">加票数：</td>
+                   <td class="spe_line" height="30" align="right"><label for="f_AddCount">加票数：</label></td>
                     <td class="spe_line"><input name="f_AddCount" id="f_AddCount" value="{AddCount}" type="text" class="input_number" style=" width: 60px;" /></td>
 
                 </tr>
