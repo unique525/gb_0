a7017a734bbb324df0bd498ad887cefd57d76883
hyyjy <8682015@163.com>
2014-08-20 10:45:54 +0800

    123

--- system_template/common/product/product_param_type_option_list.html.php
+++ system_template/common/product/product_param_type_option_list.html.php
@@ -93,34 +93,22 @@ function zTreeOnClick(event, treeId, treeNode) {
     var parentName = zTree1.getNodeByParam("id", parentId).name;
     $("#s_ParentName").text(parentName);
     $.ajax({
-        url:"default.php?secu=manage&mod=product_param_type",
-        data:{m:"one",product_param_type_id:id},
+        url:"default.php?secu=manage&mod=product_param_type_option",
+        data:{m:"one",product_param_type_option_id:id},
         dataType:"jsonp",
         jsonp:"jsonpcallback",
         success:function(data){
-            $("#f_ChannelId").val(data['ChannelId']);
             $("#f_ParentId").val(data['ParentId']);
-            $("#f_ProductParamTypeId").val(data['ProductParamTypeId']);
-            $("#s_ProductParamTypeId").text(data['ProductParamTypeId']);
-            $("#f_ParamValueType").val(data['ParamValueType']);
-            $("#f_ParamTypeName").val(data['ParamTypeName']);
+            $("#f_ProductParamTypeOptionId").val(data['ProductParamTypeOptionId']);
+            $("#s_ProductParamTypeOptionId").text(data['ProductParamTypeOptionId']);
+            $("#f_ProductParamTypeId").val(data['ProductParamTypeID']);
+            $("#f_OptionName").val(data['OptionName']);
+            $("#f_OptionName2").val(data['OptionName2']);
             $("#f_Sort").val(data['Sort']);
-            $("#f_CreateDate").val(data['CreateDate']);
             $("#img_titlepic").val("");
             var btnSubmit=$("#btnSubmit");
             btnSubmit.attr('value', '修改');
             btnSubmit.css("display","");
-            var optionHref=$("#optionHref");
-            if(data['ParamValueType']==6)
-            {
-                optionHref.css("display","");
-                optionHref.attr("href","/default.php?secu=manage&mod=product_param_type_option&m=list&channel_id="+data['ChannelId']+"&product_param_type_id="+data['ProductParamTypeId']+"&parentId="+data['ParentId']);
-            }
-            else
-            {
-                optionHref.css("display","none");
-                optionHref.attr("href","#");
-            }
         }
     });
     return true;
@@ -155,30 +143,27 @@ function addTreeNode() {
     var selectNode = zTree1.getSelectedNodes()[0];
     var parentId=selectNode.id;
     var parentName = selectNode.name;
-    $("#f_ChannelId").val(Request['channel_id']);
     $("#s_ParentName").text(parentName);
     $("#f_ParentId").val(parentId);
-    $("#f_ProductParamTypeId").val("");
-    $("#f_ParamValueType").val("0");
-    $("#s_ProductParamTypeId").text("");
-    $("#f_ParamTypeName").val("");
+    $("#f_ProductParamTypeOptionId").val("");
+    $("#s_ProductParamTypeOptionId").text("");
+    $("#f_ProductParamTypeId").val(Request['product_param_type_id']);
+    $("#f_OptionName").val("");
+    $("#f_OptionName2").val("");
     $("#f_Sort").val('0');
     var file = $("#img_titlepic");
     file.after(file.clone().val(''));//清空file类型控件
     file.remove();
-    var today = new Date();
-    var createDate = formatDate(today,"yyyy-MM-dd HH:mm:ss");
-    $("#f_CreateDate").val(createDate);
     var btnSubmit = $("#btnSubmit");
     btnSubmit.attr('value', '新增');
     btnSubmit.css("display","");
 }
 
 //用于新增返回时调用同步树节点
-function AddNodeById(sourceId,targetId,name,eName)
+function AddNodeById(sourceId,targetId,productParamTypeid,name,eName)
 {
     var parentNode = zTree1.getNodeByParam("id", targetId);
-    zTree1.addNodes(parentNode,[{ id:""+sourceId,pid:""+targetId,name:""+name,valueType:""+eName}]);
+    zTree1.addNodes(parentNode,[{ id:""+sourceId,pid:""+targetId,productParamTypeId:""+productParamTypeid,name:""+name,eName:""+eName}]);
 }
 
 //用于修改时调用同步树节点
@@ -195,7 +180,7 @@ function removeTreeNode() {
     hideRMenu();
     var treeNode = zTree1.getSelectedNodes()[0];
     var id=treeNode.id;
-    var channelId=Request['channel_id'];
+    var productParamTypeId=treeNode.productParamTypeId;
     if (treeNode) {
         if (treeNode.childs && treeNode.childs.length > 0) {
             var msg = "要删除的节点是父节点，只允许删除叶节点";
@@ -203,37 +188,37 @@ function removeTreeNode() {
         } else {
             removeTreeNodeByNode(treeNode);
             /*$.ajax({
-                url:"/default.php?secu=manage&mod=product_param_type",
+                url: "/product/index.php?a=product_param",
                 async: false,
-                data:{m:"param_count_by_id",channel_id:channelId,product_param_type_id:id},
-                dataType:"jsonp",
-                jsonp:"jsonpcallback",
-                success:function(data){
-                    var count=data['result'];
-                    if(count<0){
-                        msg="节点数据有错，请联系管理员";
+                data: {m: "paramcount", optionid: id, paramTypeId: paramTypeId},
+                dataType: "jsonp",
+                jsonp: "jsonpcallback",
+                success: function (data) {
+                    var count = data['result'];
+                    if (count < 0) {
+                        msg = "节点数据有错，请联系管理员";
                         alert(msg);
                     }
-                    else if(count>0){
-                        msg="该节点被不止一个参数引用，不能删除";
+                    else if (count > 0) {
+                        msg = "该节点被不止一个参数引用，不能删除，如果跟某个节点重复，建议用合并节点的方法处理";
                         alert(msg);
                     }
-                    else  {
+                    else {
                         removeTreeNodeByNode(treeNode);
                     }
                 }
-            }); */
+            });*/
         }
     }
 }
 
 function removeTreeNodeByNode(treeNode)
 {
-    var product_param_type_id=treeNode.id;
+    var product_param_type_option_id=treeNode.id;
     $.ajax({
-        url:"/default.php?secu=manage&mod=product_param_type",
+        url:"/default.php?secu=manage&mod=product_param_type_option",
         async: false,
-        data:{m:"delete",product_param_type_id:product_param_type_id},
+        data:{m:"delete",product_param_type_option_id:product_param_type_option_id},
         dataType:"jsonp",
         jsonp:"jsonpcallback",
         success:function(data){
@@ -248,9 +233,9 @@ function zTreeBeforeDrop(treeId, treeNodes, targetNode, moveType) {
     var pId="";
     if(moveType=="inner") pId=targetNode.id; else pId=targetNode.pId;
     $.ajax({
-        url:"/default.php?secu=manage&mod=product_param_type",
+        url:"/default.php?secu=manage&mod=product_param_type_option",
         async: false,
-        data:{m:"drag",product_param_type_id:id,parent_id:pId,param_value_type:1},
+        data:{m:"drag",product_param_type_option_id:id,parent_id:pId,param_value_type:1},
         dataType:"jsonp",
         jsonp:"jsonpcallback",
         success:function(data){
@@ -271,18 +256,87 @@ function sub()
 {
     var message="";
     var mainForm=$('#mainForm');
-    if($('#f_ParamTypeName').val() == ''){
+    if($('#f_OptionName').val() == ''){
         message +="请输入名称\r\n";
     }
     if(message!="") alert(message);
     else{
         var methodName=$("#btnSubmit").attr('value');
         var actionUrl="";
-        if(methodName=="修改") actionUrl="/default.php?secu=manage&mod=product_param_type&m=modify";
-        else if(methodName=="新增") actionUrl="/default.php?secu=manage&mod=product_param_type&m=create";
+        if(methodName=="修改") actionUrl="/default.php?secu=manage&mod=product_param_type_option&m=modify";
+        else if(methodName=="新增") actionUrl="/default.php?secu=manage&mod=product_param_type_option&m=create";
         else {alert("错误的操作类型");}
         mainForm.attr("action",actionUrl);
         mainForm.submit();
+    }
+}
+
+function setSourceNode()
+{
+    hideRMenu();
+    var treeNode = zTree1.getSelectedNodes()[0];
+    if (treeNode.childs && treeNode.childs.length > 0) {
+        var msg = "只允许选择叶节点";
+        alert(msg);
+    }
+    var id=treeNode.id;
+    var name=treeNode.name;
+    var productParamTypeId=treeNode.productParamTypeId;
+    var parentId = treeNode.pId;
+    var parentNode = zTree1.getNodeByParam("id", parentId);
+    var parentName = parentNode.name;
+    $('#sourcenodeid').val(id);
+    $('#sourcenodename').text(parentName+"_"+name);
+    $('#sourceProductParamTypeId').val(productParamTypeId);
+}
+
+function setTargetNode()
+{
+    hideRMenu();
+    var treeNode = zTree1.getSelectedNodes()[0];
+    var id=treeNode.id;
+    var name=treeNode.name;
+    var productParamTypeId=treeNode.paramTypeId;
+    var parentId = treeNode.pId;
+    var parentNode = zTree1.getNodeByParam("id", parentId);
+    var parentName = parentNode.name;
+    $('#targetnodeid').val(id);
+    $('#targetnodename').text(parentName+"_"+name);
+    $('#targetparamTypeId').val(productParamTypeId);
+}
+
+function mergeNode()
+{
+    var sourceId=$('#sourcenodeid').val();
+    var targetId=$('#targetnodeid').val();
+    var sourceParamTypeId=$('#sourceparamTypeId').val();
+    var targetParamTypeId=$('#targetparamTypeId').val();
+    var message="";
+    if(sourceId == ''){
+        message +="请选择源节点";
+    }
+    if(targetId == ''){
+        message +="请选择目标节点";
+    }
+    if(sourceParamTypeId!=targetParamTypeId){
+        message +="源节点和目标节点类型不一致，不能合并";
+    }
+    if(message!="") alert(message);
+    else{
+        $.ajax({
+            url:"/product/index.php?a=product_param",
+            async: false,
+            data:{m:"merge",sourceId:sourceId,targetId:targetId,paramTypeId:sourceParamTypeId},
+            dataType:"jsonp",
+            jsonp:"jsonpcallback",
+            success:function(data){
+                if(data['result']>0)
+                {
+                    alert("合并节点成功");
+                }
+                else {alert("合并节点失败")}
+            }
+        });
     }
 }
 
@@ -301,8 +355,10 @@ function sub()
 </div>
 
 <div id="rMenu">
-     <ul id="m_add" onclick="addTreeNode();"><li>增加节点</li></ul>
-     <ul id="m_del" onclick="removeTreeNode();"><li>删除节点</li></ul>
+    <ul id="m_add" onclick="addTreeNode();"><li>增加节点</li></ul>
+    <ul id="m_del" onclick="removeTreeNode();"><li>删除节点</li></ul>
+    <ul id="m_sourceNode" onclick="setSourceNode();"><li>选择源节点(合并用)</li></ul>
+    <ul id="m_targetNode" onclick="setTargetNode();"><li>选择目标节点(合并用)</li></ul>
 </div>
 <form id="mainForm" name="mainForm" action="" method="post"  enctype="multipart/form-data"  target='hidden_frame' >
     <div style="margin:8px;">
@@ -323,12 +379,16 @@ function sub()
                             </td>
                         </tr>
                         <tr>
-                            <td class="spe_line" ><label for="f_ParamTypeName">名称：</label></td>
+                            <td class="spe_line" ><label for="f_OptionName">名称：</label></td>
                             <td class="spe_line">
-                                <input type="text" class="input_box" id="f_ParamTypeName" name="f_ParamTypeName" value="" style=" width: 200px;font-size:14px;" maxlength="50" /><label id="s_ProductParamTypeId" ></label>
-                                <input type="hidden" id="f_ProductParamTypeId" name="f_ProductParamTypeId" value="" />
-                                <input type="hidden" id="f_ChannelId" name="f_ChannelId" value="" />
-                                <input type="hidden" id="f_CreateDate" name="f_CreateDate" value="" />
+                                <input type="text" class="input_box" id="f_OptionName" name="f_OptionName" value="" style=" width: 200px;font-size:14px;" maxlength="50" /><label id="s_ProductParamTypeOptionId" ></label>
+                                <input type="hidden" id="f_ProductParamTypeOptionId" name="f_ProductParamTypeOptionId" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="spe_line" ><label for="f_OptionName2">名称2：</label></td>
+                            <td class="spe_line">
+                                <input type="text" class="input_box" id="f_OptionName2" name="f_OptionName2" value="" style=" width: 200px;font-size:14px;" maxlength="50" />
                             </td>
                         </tr>
                         <tr>
@@ -347,22 +407,35 @@ function sub()
                             </td>
                         </tr>
                         <tr>
-                            <td class="spe_line" ><label for="f_ParamValueType">类型：</label></td>
-                            <td class="spe_line">
-                                <select id="f_ParamValueType" name="f_ParamValueType">
-                                    <option value="0">默认</option>
-                                    <option value="6">选项</option>
-                                </select>
-                                &nbsp;&nbsp;<a id="optionHref" href="#" target="_self" style="display:none">修改参数类型选项</a>
-                            </td>
-                        </tr>
-                        <tr>
                             <td class="spe_line"><label for="f_Sort">排序数字：</label></td>
                             <td class="spe_line"><input type="text" class="input_number" id="f_Sort" name="f_Sort" value="0" style=" width: 100px;font-size:14px;" maxlength="10" /> (输入数字，越大越靠前)</td>
                         </tr>
                         <tr>
                             <td colspan="2" align="right" style="padding-top:10px">
                                 <input type="button" id="btnSubmit" name="btnSubmit" class="btnSubmit" tabindex="0" onclick="sub();" style="display:none" value="" accesskey="s" />
+                            </td>
+                        </tr>
+                    </table>
+                    <table width="100%" style="padding:45px 0 0 60px;" cellspacing="0" cellpadding="0">
+                        <tr>
+                            <td class="spe_line">源节点名称：</td>
+                            <td class="spe_line">
+                                <label id="sourceNodeName" style=" width: 200px;font-size:14px;"></label>
+                                <input type="hidden" id="sourceNodeId" name="sourceNodeId" value="" />
+                                <input type="hidden" id="sourceParamTypeId" name="sourceParamTypeId" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="spe_line">目标节点名称：</td>
+                            <td class="spe_line">
+                                <label id="targetNodeName" style=" width: 200px;font-size:14px;"></label>
+                                <input type="hidden" id="targetNodeId" name="targetNodeId" value="" />
+                                <input type="hidden" id="targetParamTypeId" name="targetParamTypeId" value="" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" align="right" style="padding-top:10px">
+                                <input type="button" id="btn2" name="btn2" tabindex="0" onclick="mergeNode();" value="移动节点数据" accesskey="s" />
                             </td>
                         </tr>
                     </table>
