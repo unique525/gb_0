0db93ac891dd80f2c9b9d9ac53c69875ff7c6be6
hyyjy <8682015@163.com>
2014-08-21 17:37:30 +0800

    123

--- FrameWork1/RuleClass/DataProvider/Product/ProductParamManageData.php
+++ FrameWork1/RuleClass/DataProvider/Product/ProductParamManageData.php
@@ -30,7 +30,7 @@ class ProductParamManageData extends BaseManageData {
                 . " FROM " . self::TableName_ProductParam ." t"
                 . " LEFT OUTER JOIN" . self::TableName_ProductParamType ." t1"
                 . " ON t.".self::TableId_ProductParamType."=t1.".self::TableId_ProductParamType
-                . " WHERE t.ProductId=:ProductId and State<100"
+                . " WHERE t.ProductId=:ProductId AND t.State<100 AND t1.State<100"
                 . $order
                 . $topCount;
             $dataProperty = new DataProperty();
@@ -71,69 +71,6 @@ class ProductParamManageData extends BaseManageData {
                 . $topCount;
             $dataProperty->AddField("productId", $productId);
             $result = $this->dbOperator->GetArrayList($sql, $dataProperty);
-        }
-        return $result;
-    }
-
-    /**
-     * 根据产品参数类型Id获取产品参数条数
-     * @param int $productParamTypeId 产品参数类型Id
-     * @return int 产品参数条数
-     */
-    public function GetParamCount($productParamTypeId)
-    {
-        $result = -1;
-        if ($productParamTypeId > 0) {
-            $sql = "SELECT COUNT(*)"
-                . " FROM " . self::TableName_ProductParam
-                . " WHERE " . self::TableId_ProductParamType . "=:" . self::TableId_ProductParamType;
-            $dataProperty = new DataProperty();
-            $dataProperty->AddField("productParamTypeId", $productParamTypeId);
-            $result = $this->dbOperator->GetInt($sql, $dataProperty);
-        }
-        return $result;
-    }
-
-    /**
-     * 根据产品参数类型Id和产品参数类型选项Id获取产品参数条数
-     * @param int $productParamTypeId 产品参数类型Id
-     * @param int $productParamTypeOptionId 产品参数类型选项Id
-     * @return int 产品参数条数
-     */
-    public function GetParamCountByOptionID($productParamTypeId, $productParamTypeOptionId)
-    {
-        $result = -1;
-        if ($productParamTypeId > 0 && $productParamTypeOptionId>0) {
-            $sql = "SELECT COUNT(*)"
-                . " FROM " . self::TableName_ProductParam
-                . " WHERE " . self::TableId_ProductParamType . "=:" . self::TableId_ProductParamType . " AND ShortStringValue=:ShortStringValue";
-            $dataProperty = new DataProperty();
-            $dataProperty->AddField("productParamTypeId", $productParamTypeId);
-            $dataProperty->AddField("ShortStringValue", $productParamTypeOptionId);
-            $result = $this->dbOperator->GetInt($sql, $dataProperty);
-        }
-        return $result;
-    }
-
-    /**
-     * 批量修改产品表中产品参数类型选项值
-     * @param int $sourceId 产品参数类型源选项值
-     * @param int $targetId 产品参数类型目标选项值
-     * @param int $productParamTypeId 产品参数类型id
-     * @return int 执行结果
-     */
-    public function UpdateShortStringValue($sourceId, $targetId, $productParamTypeId)
-    {
-        $result = -1;
-        if ($sourceId > 0 && $targetId > 0 && $productParamTypeId > 0) {
-            $sql = "UPDATE " . self::TableName_ProductParam
-                . " set ShortStringValue=:targetId"
-                . " WHERE ShortStringValue=:sourceId AND " . self::TableId_ProductParamType . "=:" . self::TableId_ProductParamType;
-            $dataProperty = new DataProperty();
-            $dataProperty->AddField("sourceId", $sourceId);
-            $dataProperty->AddField("targetId", $targetId);
-            $dataProperty->AddField(self::TableId_ProductParamType, $productParamTypeId);
-            $result = $this->dbOperator->Execute($sql, $dataProperty);
         }
         return $result;
     }
