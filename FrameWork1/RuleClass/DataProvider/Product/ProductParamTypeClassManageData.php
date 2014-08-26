7e032f3fd7cf0a5c4473602d04eeb59bf8c84adc
hyyjy <8682015@163.com>
2014-08-26 16:15:52 +0800

    123

---
+++ FrameWork1/RuleClass/DataProvider/Product/ProductParamTypeClassManageData.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * 投票调查数据类
+ * @category iCMS
+ * @package iCMS_FrameWork1_RuleClass_DataProvider_ProductParamTypeClass
+ * @author hy
+ */
+class ProductParamTypeClassManageData extends BaseManageData
+{
+    /**
+     * 取得字段数据集
+     * @param string $tableName 表名
+     * @return array 字段数据集
+     */
+    public function GetFields($tableName = self::TableName_ProductParamTypeClass){
+        return parent::GetFields(self::TableName_ProductParamTypeClass);
+    }
+
+    /**
+     * 新增产品参数类型类别
+     * @param array $httpPostData $_post数组
+     * @return int 投票调查Id
+     */
+    public function Create($httpPostData)
+    {
+        $dataProperty = new DataProperty();
+        $sql = parent::GetInsertSql($httpPostData, self::TableName_ProductParamTypeClass, $dataProperty);
+        $result = $this->dbOperator->LastInsertId($sql, $dataProperty);
+        return $result;
+    }
+
+    /**
+     * 异步修改状态
+     * @param string $voteId 产品参数类型类别Id
+     * @param string $state 状态
+     * @return int 执行结果
+     */
+    public function ModifyState($voteId,$state)
+    {
+        $result = -1;
+        if ($voteId < 0) {
+            return $result;
+        }
+        $sql = "UPDATE " . self::TableName_ProductParamTypeClass . " SET State=:State WHERE ProductParamTypeClassId=:ProductParamTypeClassId;";
+        $dataProperty = new DataProperty();
+        $dataProperty->AddField("ProductParamTypeClassId", $voteId);
+        $dataProperty->AddField("State", $state);
+        $result = $this->dbOperator->Execute($sql, $dataProperty);
+        return $result;
+    }
+
+    /**
+     * 修改产品参数类型类别
+     * @param array $httpPostData $_post数组
+     * @param int $voteId 产品参数类型类别Id
+     * @return int 执行结果
+     */
+    public function Modify($httpPostData, $voteId)
+    {
+        $result = -1;
+        if ($voteId < 0) {
+            return $result;
+        }
+        $dataProperty = new DataProperty();
+        $sql = parent::GetUpdateSql($httpPostData, self::TableName_ProductParamTypeClass, self::TableId_ProductParamTypeClass, $voteId, $dataProperty);
+        $result = $this->dbOperator->Execute($sql, $dataProperty);
+        return $result;
+    }
+
+    /**
+     * 一个产品参数类型类别的信息
+     * @param int $voteId 产品参数类型类别Id
+     * @return array 产品参数类型类别一维数组
+     */
+    public function GetOne($voteId)
+    {
+        $result = null;
+        if ($voteId < 0) {
+            return $result;
+        }
+        $sql = "SELECT ProductParamTypeClassId,SiteId,ChannelId,ProductParamTypeClassTitle,State,CreateDate,BeginDate,EndDate,Sort,RecordCount,AddCount,IsCheckCode,IpMaxCount,UserMaxCount,UserScoreNum,TemplateName FROM " . self::TableName_ProductParamTypeClass . " WHERE ProductParamTypeClassId=:ProductParamTypeClassId;";
+        $dataProperty = new DataProperty();
+        $dataProperty->AddField("ProductParamTypeClassId", $voteId);
+        $result = $this->dbOperator->GetArray($sql, $dataProperty);
+        return $result;
+    }
+
+    /**
+     * 获取产品参数类型类别分页列表
+     * @param int $siteId 站点Id
+     * @param int $channelId 频道Id
+     * @param int $pageBegin 起始页码
+     * @param int $pageSize 每页记录数
+     * @param int $allCount 记录总数
+     * @param string $searchKey 查询字符
+     * @return array  产品参数类型类别列表数组
+     */
+    public function GetListForPager($siteId, $channelId, $pageBegin, $pageSize, &$allCount, $searchKey = "")
+    {
+        $result = null;
+        if ($siteId < 0 || $channelId < 0 || $pageBegin < 0 || $pageSize < 0) {
+            return $result;
+        }
+        $dataProperty = new DataProperty();
+        $dataProperty->AddField("SiteId", $siteId);
+        $dataProperty->AddField("ChannelId", $channelId);
+        $searchSql = "WHERE";
+        if (strlen($searchKey) > 0 && $searchKey != "undefined") {
+            $searchSql .= " SiteId=:SiteId AND ChannelId=:ChannelId AND (ProductParamTypeClassTitle like :searchKey1) AND";
+            $dataProperty->AddField("searchKey1", "%" . $searchKey . "%");
+        }
+        if (strlen($searchSql) > 5)
+            $searchSql = substr($searchSql, 0, strlen($searchSql) - 3);
+        else
+            $searchSql = "WHERE SiteId=:SiteId AND ChannelId=:ChannelId";
+
+        $sql = "SELECT ProductParamTypeClassId,ProductParamTypeClassTitle,State,CreateDate,BeginDate,EndDate,SiteId,ChannelId
+        FROM " . self::TableName_ProductParamTypeClass . " " . $searchSql . "
+        ORDER BY Sort DESC,ProductParamTypeClassId DESC LIMIT " . $pageBegin . "," . $pageSize . ";";
+        $result = $this->dbOperator->GetArrayList($sql, $dataProperty);
+        $sql = "SELECT COUNT(*) FROM " . self::TableName_ProductParamTypeClass . " ". $searchSql . ";";
+        $allCount = $this->dbOperator->GetInt($sql, $dataProperty);
+        return $result;
+    }
+
+}
+
+?>
---
+++ FrameWork1/RuleClass/Gen/Product/ProductParamTypeClassManageGen.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * 产品参数类型类别后台管理生成类
+ * @category iCMS
+ * @package iCMS_Rules_Gen_Product
+ * @author yanjiuyuan
+ */
+class ProductParamTypeClassManageGen extends BaseManageGen implements IBaseManageGen
+{
+
+    /**
+     * 牵引生成方法(继承接口)
+     * @return string 返回输出的HTML内容
+     */
+    public function Gen()
+    {
+        $result = "";
+        $method = Control::GetRequest("m", "");
+        switch ($method) {
+            case "create":
+                $result = self::GenCreate();
+                break;
+            case "modify_state":
+                $result = self::AsyncModifyState();
+                break;
+            case "modify":
+                $result = self::GenModify();
+                break;
+            case "list":
+                $result = self::GenList();
+                break;
+        }
+
+        $result = str_ireplace("{method}", $method, $result);
+        return $result;
+    }
+
+    /**
+     * 生成产品参数类型类别管理新增页面
+     * @return mixed|string
+     */
+    private function GenCreate()
+    {
+        $manageUserId = Control::GetManageUserId();
+        $siteId = Control::GetRequest("site_id", 0);
+        $channelId = Control::GetRequest("channel_id", 0);
+        $pageIndex = Control::GetRequest("p", 0);
+        $tempContent = Template::Load("product/product_param_type_class_deal.html", "common");
+        if ($manageUserId > 0) {
+            parent::ReplaceFirst($tempContent);
+            $productManageData = new ProductParamTypeClassManageData();
+            if (!empty($_POST)) {
+                $httpPostData = $_POST;
+                $productParamTypeClassId = $productManageData->Create($httpPostData);
+
+                //加入操作日志
+                $operateContent = 'Create ProductParamTypeClass,POST FORM:' . implode('|', $_POST) . ';\r\nResult:productParamTypeClassId:' . $productParamTypeClassId;
+                self::CreateManageUserLog($operateContent);
+
+                if ($productParamTypeClassId > 0) {
+                    //javascript 处理
+                    Control::ShowMessage(Language::Load('product', 1));
+                    $jsCode = 'parent.location.href=parent.location.href';
+                    Control::RunJavascript($jsCode);
+                } else {
+                    Control::ShowMessage(Language::Load('product', 2));
+                }
+                return "";
+            }
+            $tempContent = str_ireplace("{PageIndex}", $pageIndex, $tempContent);
+            $tempContent = str_ireplace("{CreateDate}",date("Y-m-d H:i:s"), $tempContent);
+            $tempContent = str_ireplace("{ManageUserId}", strval($manageUserId), $tempContent);
+            $tempContent = str_ireplace("{SiteId}", strval($siteId), $tempContent);
+            $tempContent = str_ireplace("{ChannelId}", strval($channelId), $tempContent);
+            $tempContent = str_ireplace("{IpMaxCount}", "10", $tempContent);
+            $fieldsOfProduct = $productManageData->GetFields();
+            parent::ReplaceWhenCreate($tempContent, $fieldsOfProduct);
+
+            $patterns = '/\{s_(.*?)\}/';
+            $tempContent = preg_replace($patterns, "", $tempContent);
+            parent::ReplaceEnd($tempContent);
+
+        }
+        return $tempContent;
+    }
+
+    /**
+     * 修改产品参数类型类别状态
+     * @return string 修改结果
+     */
+    private function AsyncModifyState()
+    {
+        //$result = -1;
+        $productParamTypeClassId = Control::GetRequest("product_param_type_class_id", 0);
+        $state = Control::GetRequest("state",0);
+        if ($productParamTypeClassId > 0) {
+            $productData = new ProductParamTypeClassManageData();
+            $result = $productData->ModifyState($productParamTypeClassId,$state);
+            //加入操作日志
+            $operateContent = 'ModifyState ProductParamTypeClass,Get FORM:' . implode('|', $_GET) . ';\r\nResult:productParamTypeClassId:' . $productParamTypeClassId;
+            self::CreateManageUserLog($operateContent);
+        } else {
+            $result = -1;
+        }
+        return Control::GetRequest("jsonpcallback","") . '({"result":"'.$result.'"})';
+    }
+
+    /**
+     * 生成产品参数类型类别修改页面
+     * @return mixed|string
+     */
+    private function GenModify()
+    {
+        $tempContent = Template::Load("product/product_param_type_class_deal.html", "common");
+        $productParamTypeClassId = Control::GetRequest("product_id", 0);
+        $pageIndex = Control::GetRequest("p", 1);
+        parent::ReplaceFirst($tempContent);
+        $productManageData = new ProductParamTypeClassManageData();
+        if ($productParamTypeClassId > 0) {
+            if (!empty($_POST)) {
+                $httpPostData = $_POST;
+                $result = $productManageData->Modify($httpPostData, $productParamTypeClassId);
+
+                //加入操作日志
+                $operateContent = 'Modify ProductParamTypeClass,POST FORM:' . implode('|', $_POST) . ';\r\nResult:productParamTypeClassId:' . $productParamTypeClassId;
+                self::CreateManageUserLog($operateContent);
+
+                if ($result > 0) {
+                    //javascript 处理
+                    Control::ShowMessage(Language::Load('product', 3));
+                    $jsCode = 'parent.location.href=parent.location.href';
+                    Control::RunJavascript($jsCode);
+                } else {
+                    Control::ShowMessage(Language::Load('product', 4));
+                }
+            }
+            $arrList = $productManageData->GetOne($productParamTypeClassId);
+            Template::ReplaceOne($tempContent, $arrList, false);
+            $tempContent = str_ireplace("{PageIndex}", strval($pageIndex), $tempContent);
+        }
+        //替换掉{s XXX}的内容
+        $patterns = '/\{s_(.*?)\}/';
+        $tempContent = preg_replace($patterns, "", $tempContent);
+        parent::ReplaceEnd($tempContent);
+        return $tempContent;
+    }
+
+    /**
+     * 产品参数类型类别管理列表页面
+     * @return mixed|string
+     */
+    private function GenList()
+    {
+
+        $templateContent = Template::Load("product/product_param_type_class_list.html", "common");
+
+        $siteId = Control::GetRequest("site_id", 0);
+        $channelId = Control::GetRequest("channel_id", 0);
+        $pageSize = Control::GetRequest("ps", 20);
+        $pageIndex = Control::GetRequest("p", 1);
+        $searchKey = Control::GetRequest("search_key", "");
+        $searchKey = urldecode($searchKey);
+
+        if ($pageIndex > 0) {
+            $pageBegin = ($pageIndex - 1) * $pageSize;
+            $tagId = "product_param_type_class_list";
+            $allCount = 0;
+            $productManageData = new ProductParamTypeClassManageData();
+            $arrList = $productManageData->GetListForPager($siteId, $channelId, $pageBegin, $pageSize, $allCount, $searchKey);
+            if (count($arrList) > 0) {
+                Template::ReplaceList($tempContent, $arrList, $tagId);
+                $styleNumber = 1;
+                $pagerTemplate = Template::Load("pager/pager_style".$styleNumber.".html","common");
+                $isJs = FALSE;
+                $navUrl = "/default.php?secu=manage&mod=product_param_type_class&m=list&site_id=$siteId&channel_id=$channelId&p={0}&ps=$pageSize";
+                $jsFunctionName = "";
+                $jsParamList = "";
+                $pagerButton = Pager::ShowPageButton($pagerTemplate, $navUrl, $allCount, $pageSize, $pageIndex, $styleNumber, $isJs, $jsFunctionName, $jsParamList);
+                Template::ReplaceList($templateContent,$arrList,$tagId);
+                $templateContent = str_ireplace("{pager_button}", $pagerButton, $templateContent);
+            } else {
+                Template::RemoveCustomTag($templateContent, $tagId);
+                $templateContent = str_ireplace("{pager_button}", Language::Load("product", 101), $templateContent);
+            }
+        }
+        parent::ReplaceEnd($templateContent);
+        return $templateContent;
+    }
+
+}
+
+?>
