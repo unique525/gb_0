c1375ddec23373d990cc804e407e6076263dfcc6
boozzh <boozzh@gmail.com>
2014-09-12 15:13:58 +0800

    manage login

---
+++ FrameWork1/RuleClass/DataProvider/User/UserOrderData.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 公共 会员订单 数据类
+ * @category iCMS
+ * @package iCMS_FrameWork1_RuleClass_DataProvider_User
+ * @author yin
+ */
+class UserOrderData extends BaseData {
+
+    /**
+     * 状态：正常
+     */
+    const STATE_NORMAL = 0;
+    /**
+     * 状态：已删
+     */
+    const STATE_REMOVED = 100;
+
+} 
