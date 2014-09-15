c2e030152ad89326463a20838a9d62222bd61f80
a525 <76349186@qq.com>
2014-09-15 17:18:20 +0800

    ww

--- system_js/manage/channel/site_and_channel.js
+++ system_js/manage/channel/site_and_channel.js
@@ -277,7 +277,7 @@ function _ChannelClick() {
 
         } else if (window.G_SelectedChannelType === 10) { //活动表单类
             window.G_TabTitle = "";
-            window.G_TabUrl = '/default.php?secu=manage&mod=custom_form&m=list&channel_id=' + window.G_SelectedChannelId;
+            window.G_TabUrl = '/default.php?secu=manage&mod=custom_form&m=list&site_id='+ window.G_NowSiteId +'&channel_id=' + window.G_SelectedChannelId;
             addTab();
         } else if (window.G_SelectedChannelType === 11) { //文字直播类
 
