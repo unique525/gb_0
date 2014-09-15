c8bce1461eb7b7d6f42130cb572755ec30d4693b
hyyjy <8682015@163.com>
2014-09-15 09:54:09 +0800

    123

---
+++ FrameWork1/RuleClass/DataProvider/Product/ProductData.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * 后台管理 产品 数据类
+ * @category iCMS
+ * @package iCMS_FrameWork1_RuleClass_DataProvider_Product
+ * @author zhangchi
+ */
+class ProductData extends BasePublicData {
+
+
+
+
+    /**
+     * 返回一行数据
+     * @param int $productId 产品id
+     * @return array|null 取得对应数组
+     */
+    public function GetOne($productId){
+        $result = null;
+        if($productId>0){
+            $sql = "SELECT * FROM " . self::TableName_Product . " WHERE ProductId=:ProductId;";
+            $dataProperty = new DataProperty();
+            $dataProperty->AddField("ProductId", $productId);
+            $result = $this->dbOperator->GetArray($sql, $dataProperty);
+        }
+        return $result;
+    }
+
+
+    /**
+     * 获取产品记录
+     * @param int $channelId 频道Id
+     * @param string $order 排序方式
+     * @param int $topCount 显示的条数
+     * @return array|null  列表数组
+     */
+    public function GetList($channelId, $order = "", $topCount = -1)
+    {
+        $result = null;
+        if ($topCount != -1)
+            $topCount = " limit " . $topCount;
+        else $topCount = "";
+        if ($channelId > 0) {
+            switch ($order) {
+                default:
+                    $order = " ORDER BY Sort DESC,Createdate DESC";
+                    break;
+            }
+            $sql = "SELECT *"
+                . " FROM " . self::TableName_Product
+                . " WHERE ChannelId=:ChannelId AND State<100"
+                . $order
+                . $topCount;
+            $dataProperty = new DataProperty();
+            $dataProperty->AddField("ChannelId", $channelId);
+            $result = $this->dbOperator->GetArrayList($sql, $dataProperty);
+        }
+        return $result;
+    }
+
+
+    /**
+     * 取得前台产品分页列表
+     * @param int $channelId 频道id
+     * @param int $pageBegin 起始记录行
+     * @param int $pageSize 页大小
+     * @param int $allCount 记录总数
+     * @param string $searchKey 查询字符
+     * @param int $searchType 查询下拉框的类别
+     * @param int $isSelf 是否只显示当前登录的管理员录入的产品
+     * @param int $manageUserId 后台管理员id
+     * @return array 产品列表数据集
+     */
+    public function GetListForPager($channelId, $pageBegin, $pageSize, &$allCount, $searchKey = "", $searchType = 0, $isSelf = 0, $manageUserId = 0)
+    {
+        $searchSql = "";
+        $dataProperty = new DataProperty();
+        $dataProperty->AddField("ChannelId", $channelId);
+        if (strlen($searchKey) > 0 && $searchKey != "undefined") {
+            if ($searchType == 0) { //产品名称
+                $searchSql = " AND (ProductName like :SearchKey)";
+                $dataProperty->AddField("SearchKey", "%" . $searchKey . "%");
+            } else if ($searchType == 1) { //简介
+                $searchSql = " AND (ProductIntro like :SearchKey)";
+                $dataProperty->AddField("SearchKey", "%" . $searchKey . "%");
+            } else if ($searchType == 2) { //发布人
+                $searchSql = " AND (ManageUserName like :SearchKey)";
+                $dataProperty->AddField("SearchKey", "%" . $searchKey . "%");
+            } else if ($searchType == 3) { //标签
+                $searchSql = " AND (ProductTag like :SearchKey)";
+                $dataProperty->AddField("SearchKey", "%" . $searchKey . "%");
+            } else { //模糊
+                $searchSql = " AND (ProductName LIKE :SearchKey1
+                                    OR ManageUserName LIKE :SearchKey2
+                                    OR ProductIntro LIKE :SearchKey3
+                                    OR ProductTag LIKE :SearchKey4)";
+                $dataProperty->AddField("SearchKey1", "%" . $searchKey . "%");
+                $dataProperty->AddField("SearchKey2", "%" . $searchKey . "%");
+                $dataProperty->AddField("SearchKey3", "%" . $searchKey . "%");
+                $dataProperty->AddField("SearchKey4", "%" . $searchKey . "%");
+            }
+        }
+        if ($isSelf === 1 && $manageUserId > 0) {
+            $conditionManageUserId = ' AND ManageUserId=' . intval($manageUserId);
+        } else {
+            $conditionManageUserId = "";
+        }
+
+        $sql = "
+            SELECT
+            ProductId,ProductNumber,ProductName,SiteId,ChannelId,
+            ProductShortName,ProductIntro,ProductTag,
+            TitlePic1UploadFileId,TitlePic2UploadFileId,TitlePic3UploadFileId,TitlePic4UploadFileId,
+            SalePrice,CreateDate,ManageUserId,ManageUserName,UserId,UserName,
+            Sort,State,RecLevel,HitCount,RecCount,FavoriteCount,QuestionCount,IsHot,IsNew,
+            SaleState,GetScore,SendPrice,SendPriceAdd,DirectUrl,MarketPrice,SaleCount,PublishDate
+            FROM
+            " . self::TableName_Product . "
+            WHERE ChannelId=:ChannelId AND State<100 " . $searchSql . " " . $conditionManageUserId . "
+            ORDER BY Sort DESC, CreateDate DESC LIMIT " . $pageBegin . "," . $pageSize . ";";
+
+        $result = $this->dbOperator->GetArrayList($sql, $dataProperty);
+
+        $sql = "SELECT count(*) FROM " . self::TableName_Product . "
+                WHERE ChannelId=:ChannelId AND State<100 " . $conditionManageUserId . " " . $searchSql . ";";
+        $allCount = $this->dbOperator->GetInt($sql, $dataProperty);
+
+        return $result;
+    }
+} 
