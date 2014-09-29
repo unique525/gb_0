dcdc49251dc6f5df9c0aeabc96be78b1886de50e
y4139 <yinluxiu@gmail.com>
2014-09-29 17:28:10 +0800

    User

--- front_js/Common.js
+++ front_js/Common.js
@@ -13,7 +13,6 @@ $(function(){
 function addCar(siteId,productId,buyCount,productPriceId,activity_product_id){
     $.ajax({
         url:"/default.php?mod=user_car&a=async_add",
-        type:"POST",
         data:{site_id:siteId,product_id:productId,buyCount:buyCount,product_price_id:productPriceId,activity_product_id:activity_product_id},
         dataType:"jsonp",
         jsonp:"jsonpcallback",
