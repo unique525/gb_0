5980fe22a1c02544e7bc68a8411b65f665c2af4d
y4139 <yinluxiu@gmail.com>
2014-10-08 16:06:25 +0800

    User

--- default.php
+++ default.php
@@ -1,6 +1,6 @@
 <?php
 define('RELATIVE_PATH', '.');
-define('PHYSICAL_PATH', realpath('.'));
+define('PHYSICAL_PATH', str_ireplace('default.php','',realpath(__FILE__)));
 define("CACHE_PATH", "cache");
 include_once(RELATIVE_PATH . '/FrameWork1/start.php');
 ?>
