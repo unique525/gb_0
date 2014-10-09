f411a80a45111c1c96cbac9d8b6a8c91d214dea2
a525 <76349186@qq.com>
2014-10-09 10:45:59 +0800

    ww

--- system_template/common/site/site_ad_js_type_0.html.php
+++ system_template/common/site/site_ad_js_type_0.html.php
@@ -1,8 +1,8 @@
 
-document.write('<div id="site_ad_{SiteAdId}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;overflow:hidden;" class="icms_site_ad site_ad_show_type_{ShowType}" title="{ShowNumber}">');
+document.write('<div id="site_ad_{SiteAdId}" idvalue="{SiteAdId}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;overflow:hidden;" class="icms_site_ad site_ad_show_type_{ShowType}" title="{ShowNumber}">');
     <icms id="site_ad_content" type="list">
         <header><![CDATA[
-            document.write('<div class="icms_ad_item {f_OpenVirtualClick}" idvalue="{f_SiteAdContentId}" id="{f_BeginDate}_{f_EndDate}" title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;">');
+            document.write('<div class="icms_ad_item open_virtual_click_{f_OpenVirtualClick} open_count_{f_OpenCount}" idvalue="{f_SiteAdContentId}" id="{f_BeginDate}_{f_EndDate}" title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;">');
                 document.write('<a href="{f_SiteAdUrl}" target="_blank" title="{f_SiteAdContentTitle}">');
                     document.write('{f_SiteAdContent}');
                 document.write('</a>');
---
+++ system_template/common/site/site_ad_js_type_4.html.php
@@ -0,0 +1,11 @@
+document.write('<div id="site_ad_{SiteAdId}" idvalue="{SiteAdId}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;overflow:hidden;display:none" class="icms_site_ad site_ad_show_type_{ShowType}" title="{ShowNumber}" >');
+    <icms id="site_ad_content" type="list">
+        <header><![CDATA[
+            document.write('<div class="icms_ad_item pull_{f_SiteAdId} open_virtual_click_{f_OpenVirtualClick} open_count_{f_OpenCount}" idvalue="{f_ResidenceTime}" id="{f_BeginDate}_{f_EndDate}_{f_SiteAdContentId}" title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;">');
+                document.write('<a href="{f_SiteAdUrl}" target="_blank" title="{f_SiteAdContentTitle}">');
+                    document.write('{f_SiteAdContent}');
+                    document.write('</a>');
+                document.write('</div>');
+            ]]></header>
+    </icms>
+    document.write('</div>');
