9b674a2e4b76449586470a29f4cd77ce610a717e
a525 <76349186@qq.com>
2014-10-31 10:22:05 +0800

    ww

--- system_template/common/site/site_ad_js_type_2.html.php
+++ system_template/common/site/site_ad_js_type_2.html.php
@@ -4,7 +4,7 @@
 document.write('<div id="site_ad_{SiteAdId}" idvalue="{SiteAdId}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;overflow:hidden;" class="icms_site_ad site_ad_show_type_{ShowType}" title="{ShowNumber}">');
     <icms id="site_ad_content" type="list">
         <header><![CDATA[
-            document.write('<div class="icms_ad_item switch1_{f_SiteAdId} switch_{f_SiteAdId}_{c_no} open_virtual_click_{f_OpenVirtualClick} open_count_{f_OpenCount}"  idvalue="{f_ResidenceTime}" id="{f_BeginDate}_{f_EndDate}_{f_SiteAdContentId}"  title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;display:none">');
+            document.write('<div class="icms_ad_item switch_{f_SiteAdId} switch_{f_SiteAdId}_{c_no} open_virtual_click_{f_OpenVirtualClick} open_count_{f_OpenCount}"  idvalue="{f_ResidenceTime}" id="{f_BeginDate}_{f_EndDate}_{f_SiteAdContentId}"  title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;display:none">');
                 document.write('<a href="{f_SiteAdUrl}" target="_blank" title="{f_SiteAdContentTitle}">');
                     document.write('{f_SiteAdContent}');
                     document.write('</a>');
@@ -20,6 +20,6 @@ document.write('<div id="site_ad_{SiteAdId}" idvalue="{SiteAdId}" style="width:{
     </icms>
 document.write('</div>');
 
-var siteId = "icms_ad_item";
-var switchClassName = "switch1_{f_SiteAdId}";
-window.runSiteAd(siteId,switchClassName);
+var siteAdId = "icms_ad_item";
+var switchClassName = "switch_{f_SiteAdId}";
+window.runSiteAd(siteAdId,switchClassName);
