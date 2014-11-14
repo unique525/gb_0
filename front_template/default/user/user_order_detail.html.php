1db653fe9441ea6fc0a52c7dd470862b148bcce1
csolt <lilac-song@163.com>
2014-11-14 11:51:45 +0800

    user_order_detail

--- front_template/default/user/user_order_detail.html.php
+++ front_template/default/user/user_order_detail.html.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>会员中心</title>
     <link href="/images/common_css.css" rel="stylesheet" type="text/css"/>
+    <link href="/images/user_layout.css" rel="stylesheet" type="text/css" />
     <style type="text/css">
         .right {
             cursor: pointer
@@ -88,82 +89,87 @@
             <td width="1" bgcolor="#D4D4D4"></td>
             <td width="1006" valign="top">
                 <div class="rightbar">
-                    <div class="rightbar2"><a href="">星滋味首页</a> >会员中心</div>
-                    <table  width="99%" align="center" border="0" cellspacing="0" cellpadding="0">
-                        <tr>
-                            <td class="spe_line" height="30" style="width:300px;text-align: right">订单编号：</td>
-                            <td class="spe_line" height="30" style="width:300px">{UserOrderNumber}</td>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">订单名：</td>
-                            <td class="spe_line" height="30">
-                                <input type="text" class="input_box" name="f_UserOrderName" value="{UserOrderName}"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">收货信息：</td>
-                            <td class="spe_line" height="30" colspan="3">
-                                {ReceivePersonName},{Address},{Mobile}
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">送货价格：</td>
-                            <td class="spe_line" height="30">￥<input type="text" class="input_price" id="SendPrice" name="f_SendPrice" value="{SendPrice}"/></td>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">送货时间：</td>
-                            <td class="spe_line" height="30">{SendTime}</td>
-                        </tr>
-                        <tr>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">订单总价：</td>
-                            <td class="spe_line" height="30">
-                                ￥<span id="display_AllPrice" class="show_price">{AllPrice}</span>
-                            </td>
-                            <td class="spe_line" height="30" style="width:100px;text-align: right">订单状态：</td>
-                            <td class="spe_line" height="30">
-                                <script type="text/javascript">document.write(FormatOrderState("{State}",{UserOrderId}));</script>
-                            </td>
-                        </tr>
-                    </table>
-                    <div style="height:30px"></div>
-                    <table class="grid" width="99%" align="center" border="0" cellspacing="0" cellpadding="0">
-                        <tr class="grid_title2" style="height:30px">
-                            <td class="spe_line" height="30" style="text-align: center">商品名</td>
-                            <td class="spe_line" height="30" style="text-align: center">商品描述</td>
-                            <td class="spe_line" height="30" style="width:100px">商品单价</td>
-                            <td class="spe_line" height="30" style="width:100px">购买价</td>
-                            <td class="spe_line" height="30" style="width:80px">数量</td>
-                            <td class="spe_line" height="30" style="width:50px">单位</td>
-                            <td class="spe_line" height="30" style="width:100px">小计</td>
-                        </tr>
-                        <icms id="user_order_product_list">
-                            <item>
-                                <![CDATA[
-                                <tr class="grid_item2" style="height:30px" id="user_order_product_{f_UserOrderProductId}">
-                                    <td class="spe_line" height="30" style="text-align: center">{f_ProductName}</td>
-                                    <td class="spe_line" height="30" style="text-align: center">{f_ProductPriceIntro}</td>
-                                    <td class="spe_line " height="30">
-                                        ￥<span class="show_price">{f_ProductPrice}</span>
-                                    </td>
-                                    <td class="spe_line" height="30">
-                                        ￥<span class="show_price">{f_SalePrice}</span>
-                                    </td>
-                                    <td class="spe_line" height="30">
-                                        {f_SaleCount}
-                                    </td>
-                                    <td class="spe_line" height="30">
-                                        {f_ProductUnit}
-                                    </td>
-                                    <td class="spe_line" height="30">
-                                        ￥<span class="UserOrderSubtotal show_price">{f_Subtotal}</span>
+                    <div class="rightbar2"><a href="">星滋味首页</a> > <a href="">会员中心</a> > <a href="/default.php?mod=user_order&a=list">我的订单</a>>{UserOrderNumber}</div>
+                    <div class="order_detail" style="padding:20px 50px;">
+                        <div class="title">基本信息</div>
+                        <div style="padding:0 20px;">
+                            <table  width="99%" align="center" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td class="spe_line infor_title">订单编号：</td>
+                                    <td class="spe_line">{UserOrderNumber}</td>
+                                    <td class="spe_line infor_title">订单名：</td>
+                                    <td class="spe_line">
+                                        <input type="text" class="input_box" name="f_UserOrderName" value="{UserOrderName}"/>
                                     </td>
                                 </tr>
-                                ]]>
-                            </item>
-                        </icms>
-                        <tr class="grid_item2">
-                            <td class="spe_line" height="30" colspan="6" align="right">总计：</td>
-                            <td class="spe_line" colspan="2" height="30" align="left">
-                                ￥<span class="show_price" id="AllSaleProductPrice"></span>
-                            </td>
-                        </tr>
-                    </table>
+                                <tr>
+                                    <td class="spe_line infor_title">收货信息：</td>
+                                    <td class="spe_line" colspan="3">
+                                        <span style="padding-right:5px;">{ReceivePersonName}</span>,<span style="padding:0 5px;">{Address}</span>,<span style="padding:0 5px;">{Mobile}</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="spe_line infor_title">送货价格：</td>
+                                    <td class="spe_line" >￥<input type="text" class="input_price" id="SendPrice" name="f_SendPrice" value="{SendPrice}"/></td>
+                                    <td class="spe_line infor_title">送货时间：</td>
+                                    <td class="spe_line">{SendTime}</td>
+                                </tr>
+                                <tr>
+                                    <td class="spe_line infor_title">订单总价：</td>
+                                    <td class="spe_line">
+                                        ￥<span id="display_AllPrice" class="show_price">{AllPrice}</span>
+                                    </td>
+                                    <td class="spe_line infor_title">订单状态：</td>
+                                    <td class="spe_line">
+                                        <span style="color:#ff3d00;"><script type="text/javascript">document.write(FormatOrderState("{State}",{UserOrderId}));</script></span>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div style="height:30px"></div>
+                        <div class="title">商品信息</div>
+                        <table class="grid product_list" width="99%" align="center" border="0" cellspacing="0" cellpadding="0">
+                            <tr class="grid_title2 list_title">
+                                <td colspan="2" class="spe_line product_name" style="text-align: center">商品名</td>
+                                <td class="spe_line product_fullprice">商品单价</td>
+                                <td class="spe_line product_cutprice">购买价</td>
+                                <td class="spe_line product_num">数量</td>
+                                <td class="spe_line product_unit">单位</td>
+                                <td class="spe_line product_finalprice">小计</td>
+                            </tr>
+                            <icms id="user_order_product_list">
+                                <item>
+                                    <![CDATA[
+                            <tr class="grid_item2" id="user_order_product_{f_UserOrderProductId}">
+                                <td class="spe_line product_pic"><img width="80px" height="80px" /></td>
+                                <td class="spe_line product_name">{f_ProductName}</td>
+                                <td class="spe_line product_fullprice">
+                                    ￥<span class="show_price">{f_ProductPrice}</span>
+                                </td>
+                                <td class="spe_line product_cutprice">
+                                    ￥<span class="show_price">{f_SalePrice}</span>
+                                </td>
+                                <td class="spe_line product_num">
+                                    {f_SaleCount}
+                                </td>
+                                <td class="spe_line product_unit">
+                                    {f_ProductUnit}
+                                </td>
+                                <td class="spe_line product_finalprice">
+                                    ￥<span class="UserOrderSubtotal show_price">{f_Subtotal}</span>
+                                </td>
+                            </tr>
+                                 ]]>
+                                </item>
+                            </icms>
+                            <tr class="grid_item2 bottom_price">
+                                <td class="spe_line" colspan="6">总计：</td>
+                                <td class="spe_line product_finalprice">
+                                    ￥<span class="show_price" id="AllSaleProductPrice"></span>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </td>
         </tr>
