c8bcd16908af8b0473cb0f8f96bec8ef58640dd3
zcoffice <boozzh@gmail.com>
2014-12-03 18:07:01 +0800

    sddd

--- front_template/default/newspaper/newspaper_page_one.html.php
+++ front_template/default/newspaper/newspaper_page_one.html.php
@@ -8,8 +8,8 @@
     <style>
         body{background:#efefef;}
         img, object { max-width: 100%;}
-        a { text-decoration:none;}
-        .page_button{list-style: none;margin:2px;padding:10px;text-align: center;float: left;background-color: #ebebeb}
+        a { text-decoration:none; color: #000000;}
+        .page_button{list-style: none;margin:2px;padding:5px;text-align:center;float: left;background-color: #D3D3D3;}
     </style>
 
     <script src="/system_js/jquery-1.9.1.min.js"></script>
@@ -67,8 +67,8 @@
         <div>
             <table cellpadding="0" cellspacing="0" width="100%" border="0">
                 <tr>
-                    <td style="text-align:center;cursor:pointer"><a href="/">首页</a>
-                        <div id="select_page" style="position:absolute;background-color:#ccc;padding-bottom:20px;display:none">
+                    <td style="text-align:center;cursor:pointer"><a style="color:#000000;" href="/">首页</a>
+                        <div id="select_page" style="position:absolute;background-color:#ebebeb;padding-bottom:20px;display:none">
                             <ul>
                                 <icms id="news_paper_page" type="list" >
                                     <item>
@@ -86,10 +86,10 @@
 
 
                     </td>
-                    <td style="text-align:center;cursor:pointer" id="pages">版面</td>
+                    <td style="text-align:center;cursor:pointer;color:#ef1b27;" id="pages" >版面</td>
                     <td style="text-align:center;"><img src="/image_02/1.jpg" alt="" id="" /></td>
                     <td style="text-align:center;cursor:pointer" id="select_date">
-                        <a href="/default.php?mod=newspaper&a=gen_select&channel_id={ChannelId}">
+                        <a style="color:#000000;" href="/default.php?mod=newspaper&a=gen_select&channel_id={ChannelId}">
                             往期回顾
                         </a></td>
                     <td style="text-align:center;"><img src="/image_02/1.jpg" alt="" id="" /></td>
--- front_template/default/newspaper/newspaper_select.html.php
+++ front_template/default/newspaper/newspaper_select.html.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>长沙晚报</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="/system_js/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css" />
     <link type="text/css" href="/system_template/default/images/jquery_ui/jquery-ui.min.css" rel="stylesheet" />
     <script src="/system_js/jquery-1.9.1.min.js"></script>
@@ -16,30 +17,31 @@
     </style>
     <script type="text/javascript">
         $(function() {
-                $( "#datepicker" ).datepicker({
-                    changeMonth: true,
-                    changeYear: true,
-                    onSelect: function(dateText, inst) {
-                        var today=new Date();
-                        var dateArray=dateText.substr(0,10).split("-");
-                        var date=new Date(dateArray[0],dateArray[1]-1,dateArray[2]);
-                        if(date<=today){
-                            window.location.href = '/default.php?mod=newspaper&a=gen_one&channel_id={ChannelId}&publish_date='+dateText;
-                        }
-
-                    },
-                    dateFormat: 'yy-mm-dd'
-                });
 
         });
         $(document).on("pageinit","#pageone",function(){
+            $( "#datepicker" ).datepicker({
+                changeMonth: true,
+                changeYear: true,
+                onSelect: function(dateText, inst) {
+                    var today=new Date();
+                    var dateArray=dateText.substr(0,10).split("-");
+                    var date=new Date(dateArray[0],dateArray[1]-1,dateArray[2]);
+                    if(date<=today){
+                        window.location.href = '/default.php?mod=newspaper&a=gen_one&channel_id={ChannelId}&publish_date='+dateText;
+                    }
 
+                },
+                dateFormat: 'yy-mm-dd'
+            });
         });
     </script>
+
 </head>
 <body>
 
 <div data-role="page" id="pageone">
+
     <div style="background:#ebebeb;">
         <div><img src="/image_02/2_05.jpg" width="100%" alt="" id="img_logo" /></div>
     </div>
@@ -48,11 +50,8 @@
             <table cellpadding="0" cellspacing="0" width="100%" border="0">
                 <tr>
                     <td style="text-align:center;"><a style="text-decoration:none;color:#333;" href="/" target="_blank">首页</a></td>
-                    <td style="text-align:center;">版面</td>
                     <td style="text-align:center;"><img src="/image_02/1.jpg" alt="" id="" /></td>
                     <td style="text-align:center;"><a style="text-decoration:none;color:#333;" href="/default.php?mod=newspaper&a=gen_select&channel_id={ChannelId}" target="_blank">往期回顾</a></td>
-                    <td style="text-align:center;"><img src="/image_02/1.jpg" alt="" id="" /></td>
-                    <td style="text-align:center;"></td>
                     <td style="text-align:center;"><img src="/image_02/1.jpg" alt="" id="" /></td>
                     <td style="text-align:center;"><img src="/image_02/2.jpg" alt="" id="" /></td>
                 </tr>
@@ -63,6 +62,5 @@
     </div>
     <div id="datepicker" style="margin:10px;width:100%"></div>
 </div>
-
 </body>
 </html>
