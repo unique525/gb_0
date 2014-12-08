1ee91c7363d124248e163e1d0590a6a468920a02
a525 <76349186@qq.com>
2014-12-08 12:03:03 +0800

    ww

--- system_template/common/site/site_ad_js_type_0.html.php
+++ system_template/common/site/site_ad_js_type_0.html.php
@@ -5,9 +5,13 @@ document.write('<div id="site_ad_{SiteAdId}" idvalue="{SiteAdId}" style="width:{
     <icms id="site_ad_content" type="list">
         <header><![CDATA[
             document.write('<div class="icms_ad_item" idvalue="{f_SiteAdContentId}" id="{f_BeginDate}_{f_EndDate}_{f_SiteAdContentId}" title="{f_SiteAdContentTitle}" style="width:{SiteAdWidth}px; height:{SiteAdHeight}px;">');
+                if("{f_SiteAdUrl}"==""){
+                    document.write('{f_SiteAdContent}');
+                }else{
                 document.write('<a class="open_virtual_click_{f_OpenVirtualClick} open_count_{f_OpenCount}" idvalue="{f_SiteAdContentId}" href="{f_SiteAdUrl}" target="_blank" title="{f_SiteAdContentTitle}">');
                     document.write('{f_SiteAdContent}');
                 document.write('</a>');
+                }
             document.write('</div>');
         ]]></header>
     </icms>
