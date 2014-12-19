e800cc6616f4ad4c9427b8bbfc2426f8e06e8a54
zcoffice <boozzh@gmail.com>
2014-12-19 14:50:21 +0800

    sddd

--- FrameWork1/RuleClass/Gen/User/UserClientGen.php
+++ FrameWork1/RuleClass/Gen/User/UserClientGen.php
@@ -5,7 +5,31 @@
  * @package iCMS_FrameWork1_RuleClass_Gen_User
  * @author zhangchi
  */
-class UserClientGen {
+class UserClientGen extends BaseClientGen implements IBaseClientGen  {
+    /**
+     * 引导方法
+     * @return string 返回执行结果
+     */
+    public function GenClient(){
+        $result = "";
+        $function = Control::GetRequest("f", "");
+        switch ($function) {
 
+            case "login":
+                $result = self::GenLogin();
+                break;
+
+            case "register":
+                $result = self::GenRegister();
+                break;
+
+        }
+        $result = str_ireplace("{function}", $function, $result);
+        return $result;
+    }
+
+    private function GenLogin(){
+
+    }
 }
 ?>
