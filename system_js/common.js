a67fa29102abfe1f191aa2e938e334be0e80895d
zhangchi <boozzh@gmail.com>
2015-02-02 17:26:22 +0800

    a

--- system_js/common.js
+++ system_js/common.js
@@ -198,58 +198,13 @@ function JsLoader() {
 
 function UrlEncode(string) {
     string = string.replace(/\r\n/g, "\n");
-    var uftText = "";
-
-    for (var n = 0; n < string.length; n++) {
-
-        var c = string.charCodeAt(n);
-
-        if (c < 128) {
-            uftText += String.fromCharCode(c);
-        }
-        else if ((c > 127) && (c < 2048)) {
-            uftText += String.fromCharCode((c >> 6) | 192);
-            uftText += String.fromCharCode((c & 63) | 128);
-        }
-        else {
-            uftText += String.fromCharCode((c >> 12) | 224);
-            uftText += String.fromCharCode(((c >> 6) & 63) | 128);
-            uftText += String.fromCharCode((c & 63) | 128);
-        }
-
-    }
-
-    return uftText;
+    return encodeURIComponent(string);
 }
 
-function UrlDecode(utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
+function UrlDecode(string) {
 
-    while (i < utftext.length) {
+    return decodeURIComponent(string);
 
-        c = utftext.charCodeAt(i);
-
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
-        }
-        else if ((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i + 1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i + 1);
-            c3 = utftext.charCodeAt(i + 2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
-
-    }
-
-    return string;
 }
 
 
