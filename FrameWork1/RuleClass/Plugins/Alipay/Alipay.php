647c919b84b987b86d51b9161fddd62021fc2249
zhangchi <boozzh@gmail.com>
2015-02-17 23:20:30 +0800

    a

--- FrameWork1/RuleClass/Plugins/Alipay/Alipay.php
+++ FrameWork1/RuleClass/Plugins/Alipay/Alipay.php
@@ -38,6 +38,36 @@ class Alipay
 
     }
 
+    public function InitWap($siteId){
+        $alipayConfig = array();
+        $siteConfigData = new SiteConfigData($siteId);
+
+        if (
+            strlen($siteConfigData->PayAlipayPartnerId) > 0 &&
+            strlen($siteConfigData->PayAlipayKey) > 0
+        ) {
+            //合作身份者id，以2088开头的16位纯数字
+            $alipayConfig['partner'] = $siteConfigData->PayAlipayPartnerId;
+            //商户的私钥（后缀是.pen）文件相对路径
+            $alipayConfig['private_key_path']	= PHYSICAL_PATH . '/FrameWork1/RuleClass/Plugins/Alipay/key/rsa_private_key.pem';
+            //支付宝公钥（后缀是.pen）文件相对路径
+            $alipayConfig['ali_public_key_path']= PHYSICAL_PATH . '/FrameWork1/RuleClass/Plugins/Alipay/key/alipay_public_key.pem';
+            //签名方式 不需修改
+            $alipayConfig['sign_type']    = strtoupper('RSA');
+
+            //字符编码格式 目前支持 gbk 或 utf-8
+            $alipayConfig['input_charset']= strtolower('utf-8');
+
+            //ca证书路径地址，用于curl中ssl校验
+            //请保证cacert.pem文件在当前文件夹目录中
+            $alipayConfig['cacert'] = PHYSICAL_PATH . "/FrameWork1/RuleClass/Plugins/Alipay/cacert.pem";
+            //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
+            $alipayConfig['transport']    = 'http';
+        }
+
+        return $alipayConfig;
+    }
+
     public function Submit(
         $siteId,
         $alipayConfig,
@@ -227,6 +257,74 @@ class Alipay
         }
 
 
+    }
+
+    public function NotifyUrlForWap($alipayConfig)
+    {
+        //计算得出通知验证结果
+        $alipayNotify = new AlipayNotify($alipayConfig);
+        $verify_result = $alipayNotify->verifyNotify();
+
+        if ($verify_result) { //验证成功
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //请在这里加上商户的业务逻辑程序代
+
+
+            //——请根据您的业务逻辑来编写程序（以下代码仅作参考）——
+
+            //获取支付宝的通知返回参数，可参考技术文档中服务器异步通知参数列表
+
+            //商户订单号
+
+            $out_trade_no = $_POST['out_trade_no'];
+
+            //支付宝交易号
+
+            $trade_no = $_POST['trade_no'];
+
+            //交易状态
+            $trade_status = $_POST['trade_status'];
+
+
+            if ($_POST['trade_status'] == 'TRADE_FINISHED') {
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+
+                self::DealUserOrder($out_trade_no, $trade_no, $trade_status);
+                //增加付款记录
+
+                //注意：
+                //该种交易状态只在两种情况下出现
+                //1、开通了普通即时到账，买家付款成功后。
+                //2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
+
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            } else if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
+                //判断该笔订单是否在商户网站中已经做过处理
+                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                //如果有做过处理，不执行商户的业务程序
+                self::DealUserOrder($out_trade_no, $trade_no, $trade_status);
+                //注意：
+                //该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
+
+                //调试用，写文本函数记录程序运行情况是否正常
+                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+            }
+
+            //——请根据您的业务逻辑来编写程序（以上代码仅作参考）——
+
+            echo "success"; //请不要修改或删除
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        } else {
+            //验证失败
+            echo "fail";
+
+            //调试用，写文本函数记录程序运行情况是否正常
+            //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
+        }
     }
 
     /**
