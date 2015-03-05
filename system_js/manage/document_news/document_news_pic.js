f935636e51633da90eaaaaf44851892ff6473a0d
a525 <76349186@qq.com>
2015-03-05 09:39:29 +0800

    ww

--- system_js/manage/document_news/document_news_pic.js
+++ system_js/manage/document_news/document_news_pic.js
@@ -1,8 +1,22 @@
 /**
  * document news pic
+
+
+<div class="pic_img_state" style="padding:3px 5px;">
+    <input class="input_box" style="width:99%" idvalue="{f_DocumentNewsPicId}" title="{f_UploadFileTitle}" id="pic_title_{f_DocumentNewsPicId}" value="{f_UploadFileTitle}" onclick="SetEventForDocumentNewsPicTitle({f_DocumentNewsPicId})">
+    </div>
+    <div class="pic_img_state" style="padding:3px 5px;">
+        <div class="keep_pic" idvalue="{f_DocumentNewsPicId}" title="{f_ShowInPicSlider}" id="FormValues_{f_UploadFileId}"></div>
+        <script type="text/javascript">ModifyShowInPicSlider({f_UploadFileId}, {f_ShowInPicSlider}, "keep_pic")</script>
+        <img class="btn_modify" src="/system_template/{template_name}/images/manage/start.jpg"
+        alt="在控件中显示" onclick="ModifyShowInPicSlider('{f_UploadFileId}', '1', 'modify_pic')"/>
+        <img class="btn_modify" src="/system_template/{template_name}/images/manage/stop.jpg"
+        alt="在控件中隐藏" onclick="ModifyShowInPicSlider('{f_UploadFileId}', '0', 'modify_pic')"/>
+        <img class="btn_modify" src="/system_template/{template_name}/images/manage/delete.jpg"
+        alt="删除" onclick="DeleteDocumentNewsPic('{f_UploadFileId}')"/>
+        <div class="update_title" style="float:right;cursor: pointer;display: none" idvalue="{f_DocumentNewsPicId}" title="{f_ShowInPicSlider}" id="update_pic_title_{f_DocumentNewsPicId}" onclick="AjaxUpdateDocumentNewsPicTitle({f_DocumentNewsPicId})" >点击修改</div>
+    </div>
  */
-
-
 /**
  * 新上传的图片加入图片管理栏
  * @param uploadFilePath 路径
@@ -22,11 +36,16 @@ function SetNewUploadPic(uploadFilePath,uploadFileId,showInPicSlider,templateNam
         var addImgLi="";
         addImgLi+='<li class="li_pic_img_item" id="UploadFileId_'+uploadFileId+'">';
         addImgLi+='<table class="pic_img_container"><tr><td><img style="cursor:pointer;" title="点击查看原始图片" class="pic_img" onclick="showOriImg(\'' + uploadFilePath + '\')" src="' + uploadFilePath + '" alt=""/></td></tr></table>';
+
+        addImgLi+='<div class="pic_img_state" style="padding:3px 5px;">';
+        addImgLi+='<input class="input_box" style="width:99%" idvalue="'+uploadFileId+'" id="pic_title_'+uploadFileId+'" value="" onclick="SetEventForDocumentNewsPicTitle('+uploadFileId+')">';
+        addImgLi+='</div>';
         addImgLi+='<div class="pic_img_state" style=";">';
         addImgLi+='<div class="create_pic" style="" idvalue="'+uploadFileId+'" title="'+showInPicSlider+'" id="FormValues_'+uploadFileId+'">'+text+'</div>';
         addImgLi+='<img class="btn_modify" src="/system_template/'+templateName+'/images/manage/start.jpg" alt="" onclick="ModifyShowInPicSlider(\''+uploadFileId+'\', \'1\',\'create_pic\')"/>';
         addImgLi+='<img class="btn_modify" src="/system_template/'+templateName+'/images/manage/stop.jpg" alt="" onclick="ModifyShowInPicSlider(\''+uploadFileId+'\', \'0\',\'create_pic\')"/>';
         addImgLi+='<img class="btn_modify" src="/system_template/'+templateName+'/images/manage/delete.jpg" alt="删除" onclick="DeleteDocumentNewsPic(\''+uploadFileId+'\')"/>';
+        addImgLi+='<div class="update_title" style="float:right;cursor: pointer;display: none" idvalue="'+uploadFileId+'" title="{f_ShowInPicSlider}" id="update_pic_title_'+uploadFileId+'" onclick="AjaxUpdateDocumentNewsPicTitle('+uploadFileId+')" >点击修改</div>';
         addImgLi+='</div>';
         addImgLi+='</li>';
         $("#new_pic_list").append(addImgLi);
@@ -147,3 +166,39 @@ function showOriImg(imagePath) {
     }
 }
 
+
+/**
+ * 设置事件(后台更新图片标题)
+ * @int uploadFileId
+ * @constructor
+ */
+function SetEventForDocumentNewsPicTitle(uploadFileId){
+    var submitButton=document.getElementById("update_pic_title_"+uploadFileId);
+    submitButton.innerHTML="点击修改";
+    submitButton.style.display="block";
+}
+/**
+ * 异步修改图片标题
+ * @int uploadFileId
+ * @constructor
+ */
+function AjaxUpdateDocumentNewsPicTitle(uploadFileId) {
+    var submitButton=document.getElementById("update_pic_title_"+uploadFileId);
+    var inputBox=document.getElementById("pic_title_"+uploadFileId);
+    var uploadFileTitle=inputBox.value;
+    if(uploadFileId>0){
+        $.ajax({
+            url: "/default.php?secu=manage&mod=upload_file&m=async_modify_upload_file_title",
+            data: {UploadFileId: uploadFileId,UploadFileTitle: uploadFileTitle},
+            type: "POST",
+            dataType: 'json',
+            success: function(result){
+                if(result>0){
+                    submitButton.innerHTML='<a style="color:green">修改成功</a>';
+                }else{
+                    submitButton.innerHTML='<a style="color:red">修改失败</a>';
+                }
+            }
+        });
+    }
+}
