74366101c906cda48923d15eadcfaa78a6a3bb81
a525 <76349186@qq.com>
2015-03-05 10:39:35 +0800

    ww

--- front_template/default/document/document_news_pic_slider_type_0.html.php
+++ front_template/default/document/document_news_pic_slider_type_0.html.php
@@ -1,5 +1,8 @@
-<div data-am-widget="slider" class="am-slider am-slider-d1"
-           data-am-flexslider="{&quot;controlNav&quot;:false,smoothHeight:true}">
+<style>
+    .am-slider-c1 .am-slider-desc {padding:2px}
+    .am-slider-c1 .am-control-nav li a.am-active {background-color: #FB5517;}
+</style>
+<div data-am-widget="slider" class="am-slider am-slider-c1" data-am-slider='{&quot;directionNav&quot;:false}' data-am-flexslider="{&quot;controlNav&quot;:true,smoothHeight:true}">
     <ul class="am-slides">
         <icms id="document_news_pic_slider_type_0" type="list">
             <item>
@@ -13,4 +16,3 @@
         </icms>
     </ul>
 </div>
-<!--,animationLoop:false,controlNav:false,directionNav:true,prevText:'Previous',nextText:'Next'-->
