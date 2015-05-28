3b2fd67ae0fd3d86f32b85e0e1cfa90e228cc018
fivetwofive <a76349186@gmail.com>
2015-05-28 09:51:55 +0800

    ww

--- FrameWork1/RuleClass/Gen/DefaultManageGen.php
+++ FrameWork1/RuleClass/Gen/DefaultManageGen.php
@@ -255,6 +255,22 @@ class DefaultManageGen extends BaseManageGen implements IBaseManageGen {
                     $visitManageGen = new VisitManageGen();
                     $result = $visitManageGen->Gen();
                     break;
+                case "template_library":
+                    $visitManageGen = new TemplateLibraryGen();
+                    $result = $visitManageGen->Gen();
+                    break;
+                case "template_library_content":
+                    $visitManageGen = new TemplateLibraryContentGen();
+                    $result = $visitManageGen->Gen();
+                    break;
+                case "template_library_channel":
+                    $visitManageGen = new TemplateLibraryChannelGen();
+                    $result = $visitManageGen->Gen();
+                    break;
+                case "template_library_channel_content":
+                    $visitManageGen = new TemplateLibraryChannelContentGen();
+                    $result = $visitManageGen->Gen();
+                    break;
                 default :
                     $result = self::GenDefault();
                     break;
