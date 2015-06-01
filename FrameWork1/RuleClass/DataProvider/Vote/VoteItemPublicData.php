16841a3e0b32c5872d0381deb395640f12b2d464
hyyjy <8682015@163.com>
2015-06-01 11:29:07 +0800

    123

--- FrameWork1/RuleClass/DataProvider/Vote/VoteItemPublicData.php
+++ FrameWork1/RuleClass/DataProvider/Vote/VoteItemPublicData.php
@@ -13,7 +13,9 @@ class VoteItemPublicData extends BasePublicData {
      * @return int 返回执行结果
      */
     public function UpdateCount($tableIdValue) {
-        $sqlStr = "update " . self::TableName_VoteItem . " set RecordCount = RecordCount+1 where VoteItemId=:VoteItemId";
+        $sqlStr = "update " . self::TableName_VoteItem . "
+        set RecordCount = RecordCount+1
+        where VoteItemId=:VoteItemId";
         $dataProperty = new DataProperty();
         $dataProperty->AddField("VoteItemId", $tableIdValue);
         $dbOperator = DBOperator::getInstance();
@@ -27,35 +29,45 @@ class VoteItemPublicData extends BasePublicData {
      * @return int 返回执行结果
      */
     public function UpdateCountBatch($VoteItemIdArr) {
+        $strSql=array();
+        $dataPropertyArr=array();
         foreach ($VoteItemIdArr as $value) {
             $VoteItemId = $value[0];  //题目ID
             $voteItemAllCount = $value[1];    //题目所属票数总和
             $dataProperty = new DataProperty();
-            $strSql[] = "update " . self::TableName_VoteItem . " set RecordCount = RecordCount+1,recordallcount = recordallcount+:voteitemallcount where VoteItemId=:VoteItemId";
+            $strSql[] = "UPDATE " . self::TableName_VoteItem . "
+            SET RecordCount = RecordCount+1,ReCordAllCount = ReCordAllCount+:VoteItemAllCount
+            WHERE VoteItemId=:VoteItemId";
             $dataProperty->AddField("VoteItemId", $VoteItemId);
-            $dataProperty->AddField("voteitemallcount", $voteItemAllCount);
-            $dataPropertyarr[] = $dataProperty;
+            $dataProperty->AddField("VoteItemAllCount", $voteItemAllCount);
+            $dataPropertyArr[] = $dataProperty;
         }
         $dbOperator = DBOperator::getInstance();
-        $result = $dbOperator->ExecuteBatch($strSql, $dataPropertyarr);
+        $result = $dbOperator->ExecuteBatch($strSql, $dataPropertyArr);
         return $result;
     }
 
     /**
-     * 根据voteId和state的值取得列表
-     * @param int $id 问卷id
+     * 根据投票调查id和状态的值取得题目列表
+     * @param int $voteId 投票调查id
      * @param int $state  题目的启用状态
-     * @return arr 返回查询到的state结果集
+     * @return array 返回题目列表数组
      */
     public function GetListByVoteID($voteId,$state = -1) {
         $dataProperty = new DataProperty();
         if ($state == -1) {
-            $sqlStr = "SELECT VoteItemId,VoteId,VoteItemTitle,Sort,State,VoteItemType,SelectNumMin,SelectNumMax FROM " . self::tablename . " WHERE voteid=:voteid";
+            $sqlStr = "SELECT
+            VoteItemId,VoteId,VoteItemTitle,Sort,State,VoteItemType,SelectNumMin,SelectNumMax
+            FROM " . self::TableName_VoteItem . "
+            WHERE VoteId=:VoteId";
         } else {
-            $sqlStr = "SELECT VoteItemId,VoteId,VoteItemTitle,Sort,State,VoteItemType,SelectNumMin,SelectNumMax FROM " . self::tablename . " WHERE voteid=:voteid and state=:state";
+            $sqlStr = "SELECT
+            VoteItemId,VoteId,VoteItemTitle,Sort,State,VoteItemType,SelectNumMin,SelectNumMax
+            FROM " . self::TableName_VoteItem . "
+            WHERE VoteId=:VoteId and state=:state";
             $dataProperty->AddField("state", $state);
         }
-        $dataProperty->AddField("voteid", $voteId);
+        $dataProperty->AddField("VoteId", $voteId);
         $dbOperator = DBOperator::getInstance();
         $result = $dbOperator->ReturnArray($sqlStr, $dataProperty);
         return $result;
