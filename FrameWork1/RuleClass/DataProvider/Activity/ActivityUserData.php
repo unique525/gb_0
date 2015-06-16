9e28c3d510da66d890675a4122082abcd32c1e5b
zhangchi <boozzh@gmail.com>
2015-06-16 10:52:30 +0800

    d

---
+++ FrameWork1/RuleClass/DataProvider/Activity/ActivityUserData.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 公共 活动会员 数据类
+ * @category iCMS
+ * @package iCMS_FrameWork1_RuleClass_DataProvider_Activity
+ * @author zhangchi
+ */
+class ActivityUserData {
+    /**
+     * 频道状态 申请 0
+     */
+    const STATE_APPLY = 0;
+    /**
+     * 频道状态 批准 10
+     */
+    const STATE_CONFIRM = 10;
+    /**
+     * 频道状态 拒绝 100
+     */
+    const STATE_REFUSE = 100;
+} 
