c1bd765f5cbdf64789bc07c5d229615774b3377d
hyyjy <8682015@163.com>
2015-07-27 18:06:02 +0800

    123

--- FrameWork1/RuleClass/DataProvider/Document/DocumentNewsClientData.php
+++ FrameWork1/RuleClass/DataProvider/Document/DocumentNewsClientData.php
@@ -134,7 +134,7 @@ class DocumentNewsClientData extends BaseClientData {
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf1 on dn.TitlePic1UploadFileId=uf1.UploadFileId
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf2 on dn.TitlePic2UploadFileId=uf2.UploadFileId
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf3 on dn.TitlePic3UploadFileId=uf3.UploadFileId
-            WHERE dn.ChannelId=:ChannelId AND dn.State<100 " . $searchSql . "
+            WHERE dn.ChannelId=:ChannelId AND dn.State=30 " . $searchSql . "
             ORDER BY dn.Sort DESC, dn.CreateDate DESC LIMIT " . $pageBegin . "," . $pageSize . ";";
 
 
@@ -271,7 +271,7 @@ class DocumentNewsClientData extends BaseClientData {
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf1 on dn.TitlePic1UploadFileId=uf1.UploadFileId
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf2 on dn.TitlePic2UploadFileId=uf2.UploadFileId
                     LEFT OUTER JOIN " .self::TableName_UploadFile." uf3 on dn.TitlePic3UploadFileId=uf3.UploadFileId
-            WHERE dn.ChannelId IN ($channelIds) AND dn.State<100 " . $searchSql . "
+            WHERE dn.ChannelId IN ($channelIds) AND dn.State=30 " . $searchSql . "
             ORDER BY dn.Sort DESC, dn.CreateDate DESC LIMIT " . $pageBegin . "," . $pageSize . ";";
 
         $result = $this->dbOperator->GetArrayList($sql, $dataProperty);
