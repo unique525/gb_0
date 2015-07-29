deed4d95f91344829ae396927152f014b7a40ed6
zhangchi <boozzh@gmail.com>
2015-07-29 16:57:30 +0800

    a

--- FrameWork1/RuleClass/DataProvider/Activity/ActivityClientData.php
+++ FrameWork1/RuleClass/DataProvider/Activity/ActivityClientData.php
@@ -89,9 +89,56 @@ class ActivityClientData extends BaseClientData {
     public function GetOne($activityId) {
         $result=-1;
         if($activityId>0){
-            $sql = "SELECT t.*,t1.ActivityClassName
+            $sql = "SELECT t.*,t1.ActivityClassName,
+
+
+                        uf1.UploadFilePath AS TitlePic1UploadFilePath,
+                        uf1.UploadFileMobilePath AS TitlePic1UploadFileMobilePath,
+                        uf1.UploadFilePadPath AS TitlePic1UploadFilePadPath,
+                        uf1.UploadFileThumbPath1 AS TitlePic1UploadFileThumbPath1,
+                        uf1.UploadFileThumbPath2 AS TitlePic1UploadFileThumbPath2,
+                        uf1.UploadFileThumbPath3 AS TitlePic1UploadFileThumbPath3,
+                        uf1.UploadFileWatermarkPath1 AS TitlePic1UploadFileWatermarkPath1,
+                        uf1.UploadFileWatermarkPath2 AS TitlePic1UploadFileWatermarkPath2,
+                        uf1.UploadFileCompressPath1 AS TitlePic1UploadFileCompressPath1,
+                        uf1.UploadFileCompressPath2 AS TitlePic1UploadFileCompressPath2,
+                        uf1.UploadFileCutPath1 AS TitlePic1UploadFileCutPath1,
+
+
+                        uf2.UploadFilePath AS TitlePic2UploadFilePath,
+                        uf2.UploadFileMobilePath AS TitlePic2UploadFileMobilePath,
+                        uf2.UploadFilePadPath AS TitlePic2UploadFilePadPath,
+                        uf2.UploadFileThumbPath1 AS TitlePic2UploadFileThumbPath1,
+                        uf2.UploadFileThumbPath2 AS TitlePic2UploadFileThumbPath2,
+                        uf2.UploadFileThumbPath3 AS TitlePic2UploadFileThumbPath3,
+                        uf2.UploadFileWatermarkPath1 AS TitlePic2UploadFileWatermarkPath1,
+                        uf2.UploadFileWatermarkPath2 AS TitlePic2UploadFileWatermarkPath2,
+                        uf2.UploadFileCompressPath1 AS TitlePic2UploadFileCompressPath1,
+                        uf2.UploadFileCompressPath2 AS TitlePic2UploadFileCompressPath2,
+                        uf2.UploadFileCutPath1 AS TitlePic2UploadFileCutPath1,
+
+
+                        uf3.UploadFilePath AS TitlePic3UploadFilePath,
+                        uf3.UploadFileMobilePath AS TitlePic3UploadFileMobilePath,
+                        uf3.UploadFilePadPath AS TitlePic3UploadFilePadPath,
+                        uf3.UploadFileThumbPath1 AS TitlePic3UploadFileThumbPath1,
+                        uf3.UploadFileThumbPath2 AS TitlePic3UploadFileThumbPath2,
+                        uf3.UploadFileThumbPath3 AS TitlePic3UploadFileThumbPath3,
+                        uf3.UploadFileWatermarkPath1 AS TitlePic3UploadFileWatermarkPath1,
+                        uf3.UploadFileWatermarkPath2 AS TitlePic3UploadFileWatermarkPath2,
+                        uf3.UploadFileCompressPath1 AS TitlePic3UploadFileCompressPath1,
+                        uf3.UploadFileCompressPath2 AS TitlePic3UploadFileCompressPath2,
+                        uf3.UploadFileCutPath1 AS TitlePic3UploadFileCutPath1
+
+
             FROM " . self::TableName_Activity . " t
             LEFT OUTER JOIN " . self::TableName_ActivityClass ." t1 ON t.ActivityClassId=t1.ActivityClassId
+
+            LEFT OUTER JOIN " .self::TableName_UploadFile." uf1 ON (t.TitlePic1UploadFileId=uf1.UploadFileId)
+            LEFT OUTER JOIN " .self::TableName_UploadFile." uf2 ON (t.TitlePic2UploadFileId=uf2.UploadFileId)
+            LEFT OUTER JOIN " .self::TableName_UploadFile." uf3 ON (t.TitlePic3UploadFileId=uf3.UploadFileId)
+
+
             WHERE ActivityId = :ActivityId ;";
             $dataProperty = new DataProperty();
             $dataProperty->AddField("ActivityId", $activityId);
