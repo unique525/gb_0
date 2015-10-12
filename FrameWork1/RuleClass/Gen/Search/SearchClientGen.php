fa5cdeac5c523444cc04954432fbf18c60aee182
zhangchi <boozzh@gmail.com>
2015-10-12 11:49:23 +0800

    a

--- FrameWork1/RuleClass/Gen/Search/SearchClientGen.php
+++ FrameWork1/RuleClass/Gen/Search/SearchClientGen.php
@@ -180,6 +180,9 @@ class SearchClientGen extends BaseClientGen implements IBaseClientGen
     {
 
         $arrList = array();
+
+
+
         foreach ($xObjects as $xObject) {
 
             $id = $xObject->s_id;
@@ -206,13 +209,13 @@ class SearchClientGen extends BaseClientGen implements IBaseClientGen
             $publishDate = $xObject->s_publish_date;
 
             $userName = $xObject->s_user_name;
-            $author = $xObject->s_author;
+            //$author = $xObject->s_author;
 
             $state = $xObject->s_state;
             $sort = $xObject->s_sort;
 
             $directUrl = $xObject->s_direct_url;
-            $content = Format::FormatJson(strip_tags($xObjects->s_content));
+            $content = Format::FormatJson(strip_tags($xObject->s_content));
             $siteUrl = $xObject->s_site_url;
             $source = $xObject->s_source;
 
@@ -241,7 +244,7 @@ class SearchClientGen extends BaseClientGen implements IBaseClientGen
                     "PublishDate" => $publishDate,
 
                     "UserName" => $userName,
-                    "Author" => $author,
+                    //"Author" => $author,
 
                     "State" => $state,
                     "Sort" => $sort,
