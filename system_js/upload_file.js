8aef629ad334bc55498f92e257c16a77fa2a6583
momo <1061809017@qq.com>
2015-11-10 18:16:47 +0800

    修改了flv选项

--- system_js/upload_file.js
+++ system_js/upload_file.js
@@ -449,7 +449,7 @@ function UploadFileFormatHtml(fileName){
             break;
         case "flv":
             url = '';
-            //url += '<scr'+'ipt type="text/javascript" src="/front_js/jwplayer.js"></scr'+'ipt>';
+            url += '<scr'+'ipt type="text/javascript" src="/front_js/jwplayer.js"></scr'+'ipt>';
             url += '<div id="mediaspace"></div>';
             url += '<scr'+'ipt type="text/javascript">';
             url += 'jwplayer("mediaspace").setup({';
